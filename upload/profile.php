aeaa67aa8129b0673079ccb2afaaf1992ebcf72e
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-08-19 13:50:02 -0400

    Restyled profiles.

--- upload/profile.php
+++ upload/profile.php
@@ -1,6 +1,7 @@
 <?php
 require("globals.php");
 $code = request_csrf_code('inbox_send');
+$code2 = request_csrf_code('cash_send');
 $_GET['user'] =
         (isset($_GET['user']) && is_numeric($_GET['user'])) ? abs(intval($_GET['user']))
                 : '';
@@ -80,122 +81,118 @@ else
         $db->free_result($enemy_q);
 		$CurrentTime=time();
 		$r['daysold']=round((($CurrentTime-$r['registertime'])/(3600 * 24)));
-		echo "<h3>Profile for {$r['username']}</h3>"; ?>
-<div class="row">
-	<div class="col-sm-4">
-		<div class="panel panel-default">
-			<div class="panel-heading">General Information</div>
-			<?php echo "
-			<div class='panel-body'>
-				<b>Name:</b> {$user_name}<br />
-				<b>User Level:</b> {$r['user_level']}<br />
-				<b>Gender:</b> {$r['gender']}<br />
-				<b>Class:</b> {$r['class']}<br />
-				<hr />
-				<b>Signed Up:</b> {$sup}<br />
-                <b>Last Active:</b> {$lon}<br />
-               <b>Last Action:</b> {$ula}<br />
-                <b>Last Login:</b> {$ull}<br />
-				<hr />
-				<b>Online:</b> {$on}<br />
-               <b>Days Old:</b> {$r['daysold']}<br />
-                <b>Location:</b> {$r['town_name']}
+		
+		$rhpperc = round($r['hp'] / $r['maxhp'] * 100);
+		echo "<h3>Profile for {$r['username']}</h3>";
+		?>
+		
+		<div class="row">
+			<div class="col-lg-2">
+				<?php
+					echo "<img src='{$r['display_pic']}' class='img-thumbnail img-responsive' width='250' height='250'><br />
+						<h3><b>{$r['username']} [{$r['userid']}]</b></h3>
+							{$r['user_level']}<br />
+							
+						Location: {$r['town_name']}<br />
+						Level: {$r['level']}<br />";
+						echo ($r['guild']) ? "Guild: <a href='#'>{$r['guild_name']}</a>" : '';
+						echo "HP: {$r['hp']}/{$r['maxhp']}<br />";
+				
+				?>
 			</div>
-			";
-			?>
-		</div>
-	</div>
-	<div class="col-sm-4">
-		<div class="panel panel-default">
-			<div class="panel-heading">Financial Information</div>
-			<?php echo "
-			<div class='panel-body'>
-				<b>Primary Currency:</b> " . number_format($r['primary_currency']) . "<br />
-				<b>Secondary Currency:</b> " . number_format($r['secondary_currency']) . "<br />
-				<b>Property:</b> {$r['house_name']}<br />
-				<hr />
-				<b>Referrals:</b> {$ref}<br />
-                <b>Friends:</b> " . number_format($friend) . "<br />
-               <b>Enemies:</b> " . number_format($enemy) . "
-			</div>
-			";
-			?>
-		</div>
-	</div>
-	<div class="col-sm-4">
-		<div class="panel panel-default">
-			<div class="panel-heading">Display Picture</div>
-			<?php 
-				echo ($r['display_pic']) ? "<img src='{$r['display_pic']}' class='img-thumbnail img-responsive' width='250' height='250'  alt='The display picture of {$r['username']}' title='The display picture of {$r['username']}' />" : 'No Image';
-				if (in_array($ir['user_level'], array('Member', 'NPC')))
-				{
-					$sh="&nbsp;";
-				}
-				else
-				{
-					$sh="<a data-toggle='collapse' href='#staffinfo'>Staff Info</a>";
-				}
-			?>
-		</div>
-	</div>
-</div>
-<div class="row">
-	<div class="col-sm-4">
-		<div class="panel panel-default">
-			<div class="panel-heading">Physical Information</div>
-			<?php echo "
-			<div class='panel-body'>
-				<b>Level:</b> {$r['level']}<br />
-				<b>Health:</b> {$r['hp']}/{$r['maxhp']}<br />";
-				echo ($r['guild']) ? "<b>Guild:</b> <a href='#'>{$r['guild_name']}</a>" : '<b>Guild:</b> N/A';
-				if ($r['fedjail'])
-				{
-					echo "<br /><span style='font-weight: bold; color: red;'>
-					In federal jail for {$r['fed_days']} day(s).
-					<br />
-					{$r['fed_reason']}
-					</span>";
-				}
-				if (user_infirmary($r['userid']))
-				{
-					$InfirmaryRemain=round((($r['infirmary_out'] - $CurrentTime) / 60), 2);
-					echo "
-					<br />
-					<span style='font-weight: bold; color: red;'>
-					In the infirmary for {$InfirmaryRemain} minutes.
-					<br />
-					{$r['infirmary_reason']}
-					</span>";
-				}
-				if (user_dungeon($r['userid']))
-				{
-					$DungeonRemain=round((($r['dungeon_out'] - $CurrentTime) / 60), 2);
-					echo "
-					<br />
-					<span style='font-weight: bold; color: red;'>
-					In the dungeon for {$DungeonRemain} minutes.
-					<br />
-					{$r['dungeon_reason']}
-					</span>";
-				}
-			?>
-		</div>
-	</div>
-</div>
-	<div class="col-sm-4">
-		<div class="panel panel-default">
-			<div class="panel-heading">Links</div>
-			<?php echo "
-			<div class='panel-body'>
-				<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal' data-whatever='Admin'>Send {$r['username']} a Message</button>";
-					?>
-					<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+			<div class="col-lg-10">
+				<ul class="nav nav-tabs">
+				  <li class="active"><a data-toggle="tab" href="#info">Phyiscal Information</a></li>
+				  <li><a data-toggle="tab" href="#actions">Actions</a></li>
+				  <li><a data-toggle="tab" href="#financial">Financial Information</a></li>
+				  <?php
+					if (!in_array($ir['user_level'], array('Member', 'NPC')))
+					{
+					  echo "<li><a data-toggle='tab' href='#staff'>Staff</a></li>";
+					}
+				  ?>
+				</ul>
+				<br />
+				<div class="tab-content">
+				  <div id="info" class="tab-pane fade in active">
+					<p>
+						<?php
+						echo
+						"
+						<table class='table table-bordered table-responsive'>
+							<tr>
+								<th width='25%'>Gender</th>
+								<td>{$r['gender']}</td>
+							</tr>
+							<tr>
+								<th>Class</th>
+								<td>{$r['class']}</td>
+							</tr>
+							<tr>
+								<th>Signed Up</th>
+								<td>{$sup}</td>
+							</tr>
+							<tr>
+								<th>Last Action</th>
+								<td>{$ula}</td>
+							</tr>
+							<tr>
+								<th>Last Login</th>
+								<td>{$ull}</td>
+							</tr>
+							<tr>
+								<th>Age</th>
+								<td>{$r['daysold']} Days Old</td>
+							</tr>
+						</table>";
+						if (user_infirmary($r['userid']))
+						{
+							$InfirmaryRemain=round((($r['infirmary_out'] - $CurrentTime) / 60), 2);
+							echo "
+							<br />
+							<span style='font-weight: bold; color: red;'>
+							In the infirmary for {$InfirmaryRemain} minutes.
+							<br />
+							{$r['infirmary_reason']}
+							</span>";
+						}
+						if (user_dungeon($r['userid']))
+						{
+							$DungeonRemain=round((($r['dungeon_out'] - $CurrentTime) / 60), 2);
+							echo "
+							<br />
+							<span style='font-weight: bold; color: red;'>
+							In the dungeon for {$DungeonRemain} minutes.
+							<br />
+							{$r['dungeon_reason']}
+							</span>";
+						}
+						if ($r['fedjail'])
+						{
+							echo "<br /><span style='font-weight: bold; color: red;'>
+							In federal jail for {$r['fed_days']} day(s).
+							<br />
+							{$r['fed_reason']}
+							</span>";
+						}
+						?>
+					</p>
+				  </div>
+				  <div id="actions" class="tab-pane fade">
+				  <?php echo "<button type='button' class='btn btn-default' data-toggle='modal' data-target='#message' data-whatever='Admin'>Send {$r['username']} a Message</button>"; ?>
+				  <?php echo "<br /><br /><button type='button' class='btn btn-default' data-toggle='modal' data-target='#cash' data-whatever='Admin'>Send {$r['username']} {$lang['INDEX_PRIMCURR']}</button>
+				  <br /><br /><form action='attack.php'>
+					<input type='hidden' name='user' value='{$r['userid']}'>
+					<input type='submit' class='btn btn-danger' value='Attack {$r['username']}'>
+					</form>
+				  "; ?>
+					<div class="modal fade" id="message" tabindex="-1" role="dialog" aria-labelledby="Sending a Message">
 					  <div class="modal-dialog" role="document">
 						<div class="modal-content">
 						  <div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 							<div id="success"></div>
-							<h4 class="modal-title" id="ModalLabel">New message</h4>
+							<h4 class="modal-title" id="ModalLabel"><?php echo "Sending {$r['username']} a Message"; ?></h4>
 						  </div>
 						  <div class="modal-body">
 							<form id="mailpopupForm" name="mailpopupForm" action="js/script/sendmail.php">
@@ -222,66 +219,90 @@ else
 						</div>
 					  </div>
 					</div>
+					<div class="modal fade" id="cash" tabindex="-1" role="dialog" aria-labelledby="Sending Cash">
+					  <div class="modal-dialog" role="document">
+						<div class="modal-content">
+						  <div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							<div id="success2"></div>
+							<h4 class="modal-title" id="ModalLabel"><?php echo "Sending {$r['username']} {$lang['INDEX_PRIMCURR']}"; ?></h4>
+						  </div>
+						  <div class="modal-body">
+							<form id="cashpopupForm" name="cashpopupForm" action="js/script/sendcash.php">
+							  <div class="form-group">
+								<div id="result2"></div>
+								<input type="hidden" name="sendto" required="1" value="<?php echo $r['userid']; ?>" id="recipient-name">
+							  </div>
+							  <div class="form-group">
+								<label for="message-text" class="control-label">Cash:</label>
+								<input type='number' min='0' max="<?php echo $ir['primary_currency']; ?>" class="form-control" name="cash" required="1" id="message-text"></textarea>
+							  </div>
+							
+						  </div>
+						  <div class="modal-footer">
+						  <?php
+						  echo"
+							<input type='hidden' name='verf' value='{$code2}' />";
+							?>
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+							<input type="submit" value="Send Cash" id="sendcash" class="btn btn-primary">
+							</form>
+						  </div>
+						</div>
+					  </div>
+					</div>
+				  </div>
+				  <div id="financial" class="tab-pane fade">
 					<?php
-					echo"
-					<br /><br />
-					[<a href='sendcash.php?ID={$r['userid']}'>Send {$r['username']} Cash</a>]
-					<br /><br />
-					[<a href='attack.php?user={$r['userid']}'>Attack {$r['username']}</a>]
-					<br /><br />
-					[<a href='contactlist.php?action=add&ID={$r['userid']}'>Add {$r['username']} to Contacts</a>]";
+						echo
+						"
+						<table class='table table-bordered table-responsive'>
+							<tr>
+								<th width='25%'>Gender</th>
+								<td> " . number_format($r['primary_currency']) . "</td>
+							</tr>
+							<tr>
+								<th>Class</th>
+								<td>" . number_format($r['secondary_currency']) . "</td>
+							</tr>
+							<tr>
+								<th>Signed Up</th>
+								<td>{$r['house_name']}</td>
+							</tr>
+							<tr>
+								<th>Last Action</th>
+								<td>" . number_format($ref) . "</td>
+							</tr>
+							<tr>
+								<th>Last Login</th>
+								<td>" . number_format($friend) . "</td>
+							</tr>
+							<tr>
+								<th>Age</th>
+								<td>" . number_format($enemy) . "</td>
+							</tr>
+						</table>";
+						
+						?>
+				  </div>
+				  <?php
+				  echo '<div id="staff" class="tab-pane fade in">';
 					if (!in_array($ir['user_level'], array('Member', 'NPC')))
 					{
-						echo "
-					<br /><br />
-					[<a href='jailuser.php?userid={$r['userid']}'>Fed Jail {$r['username']}</a>]
-					<br /><br />
-					[<a href='mailban.php?userid={$r['userid']}'>Mail Ban {$r['username']}</a>]
-					   ";
-					}
-					if ($ir['vip_days'] > 0)
-					{
-						echo "
-					<br /><br />
-					[<a href='friendslist.php?action=add&ID={$r['userid']}'>Add {$r['username']} to Friends List</a>]
-					<br /><br />
-					[<a href='blacklist.php?action=add&ID={$r['userid']}'>Add {$r['username']} to Enemies List</a>]
-					<br />
-					   ";
-					}
-					echo"
-			</div>
-			";
-			?>
-		</div>
-	</div>
-	<?php
-	if (!in_array($ir['user_level'], array('Member', 'NPC')))
-	{
-		?>
-	<div class="col-sm-4">
-		<div class="panel-group">
-			<div class="panel panel-default">
-				<div class="panel-heading"><?php echo $sh; ?></div>
-				<div id="staffinfo" class="panel-collapse collapse">
-			<?php echo "
-			<div class='panel-body'>";
-				
-					$curl = curl_init();
+						
+						$curl = curl_init();
 
-					curl_setopt_array($curl, array(
-						CURLOPT_URL => "https://api.fraudguard.io/ip/$r[loginip]",
-						CURLOPT_USERPWD => "{$set['FGUsername']}:{$set['FGPassword']}",
-						CURLOPT_SSL_VERIFYPEER => false,
-						CURLOPT_RETURNTRANSFER => true
-					));
+						curl_setopt_array($curl, array(
+							CURLOPT_URL => "https://api.fraudguard.io/ip/$r[loginip]",
+							CURLOPT_USERPWD => "{$set['FGUsername']}:{$set['FGPassword']}",
+							CURLOPT_SSL_VERIFYPEER => false,
+							CURLOPT_RETURNTRANSFER => true
+						));
 
-					$resp = curl_exec($curl);
-					curl_close($curl);
-					$fg=json_decode($resp,true);
-					echo "
-					<h3>Internet Info</h3>
-					<table class='table table-bordered'>
+						$resp = curl_exec($curl);
+						curl_close($curl);
+						$fg=json_decode($resp,true);
+						echo "<table class='table table-bordered'>
 							<tr>
 								<th width='33%'>Data</th>
 								<th>Output</th>
@@ -312,7 +333,6 @@ else
 								<td>{$fg['risk_level']}</td>
 							</tr>
 					</table>
-
 					<form action='staffnotes.php' method='post'>
 						Staff Notes:
 						<br />
@@ -322,20 +342,16 @@ else
 						<br />
 						<input type='hidden' name='ID' value='{$_GET['user']}' />
 						<input type='submit' class='btn btn-default' value='Update Notes About {$r['username']}' />
-					</form>
-					   ";
-
-			echo"</div>
-			";
-			?>
+					</form>";
+					}
+					?>
+				  
+				  </div>
 				</div>
 			</div>
 		</div>
-		</div>
-		</div>
-<?php
+		<?php
 		}
-	}
 }
 function checkblank($in)
 {
