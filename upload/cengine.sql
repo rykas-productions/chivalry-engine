e6c0b413d18157a6f3441ecf86b52ab1199edab6
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-08-19 14:27:10 -0400

    Cleared unneeded queries

--- upload/cengine.sql
+++ upload/cengine.sql
@@ -220,14 +220,7 @@ CREATE TABLE IF NOT EXISTS `guild` (
   `guild_level` int(11) unsigned NOT NULL,
   `guild_xp` int(11) unsigned NOT NULL,
   PRIMARY KEY (`guild_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `guild`
---
-
-INSERT INTO `guild` (`guild_id`, `guild_town_id`, `guild_owner`, `guild_coowner`, `guild_primcurr`, `guild_seccurr`, `guild_hasarmory`, `guild_capacity`, `guild_name`, `guild_desc`, `guild_level`, `guild_xp`) VALUES
-(1, 1, 1, 1, 1, 1, 'false', 5, 'test', 'test', 4, 1);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -306,13 +299,6 @@ CREATE TABLE IF NOT EXISTS `login_attempts` (
   `timestamp` int(11) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `login_attempts`
---
-
-INSERT INTO `login_attempts` (`ip`, `userid`, `timestamp`) VALUES
-('192.168.128.113', 6, 1466643890);
-
 -- --------------------------------------------------------
 
 --
@@ -360,14 +346,7 @@ CREATE TABLE IF NOT EXISTS `newspaper_ads` (
   `news_owner` int(11) unsigned NOT NULL,
   `news_text` text NOT NULL,
   PRIMARY KEY (`news_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `newspaper_ads`
---
-
-INSERT INTO `newspaper_ads` (`news_id`, `news_cost`, `news_start`, `news_end`, `news_owner`, `news_text`) VALUES
-(1, 125000, 0, 4054546460, 1, 'hi');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -451,8 +430,8 @@ INSERT INTO `settings` (`setting_id`, `setting_name`, `setting_value`) VALUES
 (2, 'RegistrationCaptcha', 'OFF'),
 (3, 'HTTPS_Support', 'false'),
 (4, 'AttackEnergyCost', '100'),
-(5, 'FGPassword', 'h8z7abKWPJjI10r9'),
-(6, 'FGUsername', '8WDgqcFYA0WPc3J5'),
+(5, 'FGPassword', 'update'),
+(6, 'FGUsername', 'update'),
 (7, 'MaxAttacksPerSession', '100');
 
 -- --------------------------------------------------------
