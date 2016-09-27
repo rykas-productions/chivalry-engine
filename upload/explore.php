925671b32aad4be7d6ebe1900fdb9d5d6c8b3bec
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-09-27 13:58:23 -0400

    Mobile optimization

--- upload/explore.php
+++ upload/explore.php
@@ -5,7 +5,7 @@ if (user_infirmary($ir['userid']) == true)
 	alert('danger',"{$lang["GEN_INFIRM"]}","{$lang['ERRDE_EXPLORE']}");
 	die($h->endpage());
 }
-echo"<h4>{$lang['EXPLORE_INTRO']}</h4><ul class='nav nav-tabs'>
+echo"<h4>{$lang['EXPLORE_INTRO']}</h4><ul class='nav nav-tabs nav-justified'>
 	<li><a data-toggle='tab' href='#SHOPS'>{$lang['EXPLORE_SHOP']}</a></li>
 	<li><a data-toggle='tab' href='#FD'>{$lang['EXPLORE_FD']}</a></li>
 	<li><a data-toggle='tab' href='#HL'>{$lang['EXPLORE_HL']}</a></li>
@@ -20,12 +20,12 @@ echo"<h4>{$lang['EXPLORE_INTRO']}</h4><ul class='nav nav-tabs'>
 	<div id='SHOPS' class='tab-pane fade in'>
 		<div class='panel panel-default'>
 			<div class='panel-body'>
-				<a href='shop.php'>{$lang['EXPLORE_LSHOP']}</a><br />
-				<a href='poshop.php'>{$lang['EXPLORE_POSHOP']}</a><br />
-				<a href='market.php'>{$lang['EXPLORE_IMARKET']}</a><br />
-				<a href='auction.php'>{$lang['EXPLORE_IAUCTION']}</a><br />
-				<a href='trade.php'>{$lang['EXPLORE_TRADE']}</a><br />
-				<a href='auction.php'>{$lang['EXPLORE_SCMARKET']}</a><br />	
+				<a href='#'>{$lang['EXPLORE_LSHOP']}</a><br />
+				<a href='#'>{$lang['EXPLORE_POSHOP']}</a><br />
+				<a href='#'>{$lang['EXPLORE_IMARKET']}</a><br />
+				<a href='#'>{$lang['EXPLORE_IAUCTION']}</a><br />
+				<a href='#'>{$lang['EXPLORE_TRADE']}</a><br />
+				<a href='#'>{$lang['EXPLORE_SCMARKET']}</a><br />	
 			</div>
 		</div>
 	</div>
@@ -33,16 +33,16 @@ echo"<h4>{$lang['EXPLORE_INTRO']}</h4><ul class='nav nav-tabs'>
 		<div class='panel panel-default'>
 			<div class='panel-body'>
 				<a href='bank.php'>{$lang['EXPLORE_BANK']}</a><br />
-				<a href='estate.php'>{$lang['EXPLORE_ESTATES']}</a><br />
+				<a href='#'>{$lang['EXPLORE_ESTATES']}</a><br />
 			</div>
 		</div>
 	</div>
 	<div id='HL' class='tab-pane fade'>
 		<div class='panel panel-default'>
 			<div class='panel-body'>
-				<a href='mine.php'>{$lang['EXPLORE_MINE']}</a><br />
-				<a href='forest.php'>{$lang['EXPLORE_WC']}</a><br />
-				<a href='farm.php'>{$lang['EXPLORE_FARM']}</a><br />
+				<a href='#'>{$lang['EXPLORE_MINE']}</a><br />
+				<a href='#'>{$lang['EXPLORE_WC']}</a><br />
+				<a href='#'>{$lang['EXPLORE_FARM']}</a><br />
 			</div>
 		</div>
 	</div>
@@ -54,15 +54,16 @@ echo"<h4>{$lang['EXPLORE_INTRO']}</h4><ul class='nav nav-tabs'>
 				<a href='fedjail.php'>{$lang['EXPLORE_FED']}</a><br />
 				<a href='stats.php'>{$lang['EXPLORE_STATS']}</a><br />
 				<a href='playerreport.php'>{$lang['EXPLORE_REPORT']}</a><br />
+				<a href='announcements.php'>{$lang['EXPLORE_ANNOUNCEMENTS']}</a><br />
 			</div>
 		</div>
 	</div>
 	<div id='GAMES' class='tab-pane fade'>
 		<div class='panel panel-default'>
 			<div class='panel-body'>
-				<a href='rroulette.php'>{$lang['EXPLORE_RR']}</a><br />
-				<a href='hlow.php'>{$lang['EXPLORE_HILO']}</a><br />
-				<a href='roulette.php'>{$lang['EXPLORE_ROULETTE']}</a><br />
+				<a href='#'>{$lang['EXPLORE_RR']}</a><br />
+				<a href='#'>{$lang['EXPLORE_HILO']}</a><br />
+				<a href='#'>{$lang['EXPLORE_ROULETTE']}</a><br />
 			</div>
 		</div>
 	</div>
@@ -79,8 +80,8 @@ echo"<h4>{$lang['EXPLORE_INTRO']}</h4><ul class='nav nav-tabs'>
 				<a href='dungeon.php'>{$lang['EXPLORE_DUNG']}</a><br />
 				<a href='infirmary.php'>{$lang['EXPLORE_INFIRM']}</a><br />
 				<a href='gym.php'>{$lang['EXPLORE_GYM']}</a><br />
-				<a href='job.php'>{$lang['EXPLORE_JOB']}</a><br />
-				<a href='academy.php'>{$lang['EXPLORE_ACADEMY']}</a><br />
+				<a href='#'>{$lang['EXPLORE_JOB']}</a><br />
+				<a href='#'>{$lang['EXPLORE_ACADEMY']}</a><br />
 			</div>
 		</div>
 	</div>
