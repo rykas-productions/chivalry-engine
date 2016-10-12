93e00d1e0c57687ae16a476a7d3dc0b198445997
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-10-12 16:13:24 -0400

    Updated build number to 0.0.29

--- upload/installer.php
+++ upload/installer.php
@@ -3,8 +3,8 @@ if (file_exists('./installer.lock'))
 {
     exit;
 }
-$Version=('0.0.28');
-$Build=('0028');
+$Version=('0.0.29');
+$Build=('0029');
 define('MONO_ON', 1);
 session_name('CENGINE');
 session_start();
