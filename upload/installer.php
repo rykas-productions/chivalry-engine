1a9a4f574c4d2aa483ee6f711c0ddce4bf629f93
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-12-18 15:19:04 -0500

    Update installer.php

--- upload/installer.php
+++ upload/installer.php
@@ -3,8 +3,8 @@ if (file_exists('./installer.lock'))
 {
     exit;
 }
-$Version=('0.0.32');
-$Build=('0032');
+$Version=('0.0.33');
+$Build=('0033');
 define('MONO_ON', 1);
 session_name('CENGINE');
 session_start();
