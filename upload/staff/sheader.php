ad268a9ce4db567c82d6abfd962ff61934310fda
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-12-19 12:32:01 -0500

    Forgot to default navbar.

--- upload/staff/sheader.php
+++ upload/staff/sheader.php
@@ -64,7 +64,7 @@ class headers
 <body>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
