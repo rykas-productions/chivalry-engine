6807240d0868bfd178dfb23b386bd682e7546365
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-12-21 19:56:45 -0500

    Uninclude NPCs

--- upload/stats/stats.php
+++ upload/stats/stats.php
@@ -8,26 +8,26 @@
 */
 
 //Select count of players with and without a bank account
-$NotOwnedBank=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `bank` = '-1'"));
-$OwnedBank=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `bank` > '-1'"));
+$NotOwnedBank=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `bank` = '-1' AND `user_level` != 'NPC'"));
+$OwnedBank=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `bank` > '-1' AND `user_level` != 'NPC'"));
 
 //Select count of players's gender
-$Male=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `gender` = 'Male'"));
-$Female=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `gender` = 'Female'"));
+$Male=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `gender` = 'Male' AND `user_level` != 'NPC'"));
+$Female=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `gender` = 'Female' AND `user_level` != 'NPC'"));
 
 //Select count of players's class
-$Warrior=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `class` = 'Warrior'"));
-$Rogue=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `class` = 'Rogue'"));
-$Defender=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `class` = 'Defender'"));
+$Warrior=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `class` = 'Warrior' AND `user_level` != 'NPC'"));
+$Rogue=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `class` = 'Rogue' AND `user_level` != 'NPC'"));
+$Defender=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `class` = 'Defender' AND `user_level` != 'NPC'"));
 
 //Select the Total Primary Currency in the game.
-$TotalPrimaryCurrency=$db->fetch_single($db->query("SELECT SUM(`primary_currency`) FROM `users`"));
+$TotalPrimaryCurrency=$db->fetch_single($db->query("SELECT SUM(`primary_currency`) FROM `users` WHERE `user_level` != 'NPC'"));
 
 //Select the Total Secondary Currency in the game.
-$TotalSecondaryCurrency=$db->fetch_single($db->query("SELECT SUM(`secondary_currency`) FROM `users`"));
+$TotalSecondaryCurrency=$db->fetch_single($db->query("SELECT SUM(`secondary_currency`) FROM `users` WHERE `user_level` != 'NPC'"));
 
 //Select total count of register users.
-$TotalUserCount=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users`"));
+$TotalUserCount=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `user_level` != 'NPC'"));
 
 //Figure out average primary currency per player by dividing the total primary currency by the total amount of users
 //then round up.
