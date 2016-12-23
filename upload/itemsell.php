00588e3b16f9f4f83f94665fea992add47aeccd7
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-12-23 16:30:28 -0500

    Respect CanSellToGame permission

--- upload/itemsell.php
+++ upload/itemsell.php
@@ -3,95 +3,98 @@ require('globals.php');
 $_GET['ID'] = (isset($_GET['ID']) && is_numeric($_GET['ID'])) ? abs(intval($_GET['ID'])) : '';
 $_POST['qty'] = (isset($_POST['qty']) && is_numeric($_POST['qty'])) ? abs(intval($_POST['qty'])) : '';
 echo "<h3>{$lang['ITEM_SELL_INFO']}</h3><hr />";
-if (!empty($_POST['qty']) && !empty($_GET['ID']))
+if (permission('CanSellToGame',$userid) == true)
 {
-    $id =
-            $db->query(
-                    "SELECT `inv_qty`, `itmsellprice`, `itmid`, `itmname`
-                     FROM `inventory` AS `iv`
-                     INNER JOIN `items` AS `it`
-                     ON `iv`.`inv_itemid` = `it`.`itmid`
-                     WHERE `iv`.`inv_id` = {$_GET['ID']}
-                     AND `iv`.`inv_userid` = {$userid}
-                     LIMIT 1");
-    if ($db->num_rows($id) == 0)
-    {
-        alert('danger',"{$lang['ITEM_SELL_ERROR1_TITLE']}","{$lang['ITEM_SELL_ERROR1']}");
-    }
-    else
-    {
-        $r = $db->fetch_row($id);
-        if (!isset($_POST['verf']) || !verify_csrf_code("sellitem_{$_GET['ID']}", stripslashes($_POST['verf'])))
-        {
-            alert('danger',"{$lang["CSRF_ERROR_TITLE"]}","{$lang["CSRF_ERROR_TEXT"]}");
-            die($h->endpage());
-        }
-        if ($_POST['qty'] > $r['inv_qty'])
-        {
-            alert('danger',"{$lang['ERROR_INVALID']}","{$lang['ITEM_SELL_BAD_QTY']}");
-        }
-        else
-        {
-            $price = $r['itmsellprice'] * $_POST['qty'];
-            item_remove($userid, $r['itmid'], $_POST['qty']);
-            $db->query(
-                    "UPDATE `users`
-                     SET `primary_currency` = `primary_currency` + {$price}
-                     WHERE `userid` = $userid");
-            $priceh = number_format($price);
-			alert('success',"{$lang['ERROR_SUCCESS']}","{$lang['ITEM_SELL_SUCCESS1']} {$_POST['qty']} {$r['itmname']}{$lang['ITEM_SELL_SUCCESS2']} {$priceh} {$lang['INDEX_PRIMCURR']}.");
-            $is_log =  $db->escape("{$ir['username']} sold {$_POST['qty']} {$r['itmname']}(s) for {$priceh}");
-            $api->SystemLogsAdd($userid,'itemsell',$is_log);
-        }
-    }
-    $db->free_result($id);
-}
-else if (!empty($_GET['ID']) && empty($_POST['qty']))
-{
-    $id =
-            $db->query(
-                    "SELECT `inv_qty`, `itmname`
-                     FROM `inventory` AS `iv`
-                     INNER JOIN `items` AS `it`
-                     ON `iv`.`inv_itemid` = `it`.`itmid`
-                     WHERE `iv`.`inv_id` = {$_GET['ID']}
-                     AND `iv`.`inv_userid` = {$userid}
-                     LIMIT 1");
-    if ($db->num_rows($id) == 0)
-    {
-        alert('danger',"{$lang['ITEM_SELL_ERROR1_TITLE']}","{$lang['ITEM_SELL_ERROR1']}");
-    }
-    else
-    {
-        $r = $db->fetch_row($id);
-        $code = request_csrf_code("sellitem_{$_GET['ID']}");
-        echo "
-		<b>{$lang['ITEM_SELL_FORM1']} {$r['itmname']}(s) {$lang['ITEM_SELL_FORM2']} {$r['inv_qty']} {$lang['ITEM_SELL_FORM3']}</b>
-		<br />
-		<form action='?ID={$_GET['ID']}' method='post'>
-			<table class='table table-bordered'>
-				<tr>
-					<th>
-						{$lang['STAFF_ITEM_GIVE_FORM_QTY']}
-					</th>
-					<td>
-						<input type='text' class='form-control' name='qty' value='{$r['inv_qty']}' />
-					</td>
-				</tr>
-				<tr>
-					<td colspan='2'>
-						<input type='submit' class='btn btn-default' value='{$lang['ITEM_SELL_BTN']}' />
-					</td>
-				</tr>
-				<input type='hidden' name='verf' value='{$code}' />
-			</table>
-		</form>
-   		";
-    }
-    $db->free_result($id);
-}
-else
-{
-    alert('danger',"{$lang['ERROR_GENERIC']}","{$lang['GEN_IUOF']}");
+	if (!empty($_POST['qty']) && !empty($_GET['ID']))
+	{
+		$id =
+				$db->query(
+						"SELECT `inv_qty`, `itmsellprice`, `itmid`, `itmname`
+						 FROM `inventory` AS `iv`
+						 INNER JOIN `items` AS `it`
+						 ON `iv`.`inv_itemid` = `it`.`itmid`
+						 WHERE `iv`.`inv_id` = {$_GET['ID']}
+						 AND `iv`.`inv_userid` = {$userid}
+						 LIMIT 1");
+		if ($db->num_rows($id) == 0)
+		{
+			alert('danger',"{$lang['ITEM_SELL_ERROR1_TITLE']}","{$lang['ITEM_SELL_ERROR1']}");
+		}
+		else
+		{
+			$r = $db->fetch_row($id);
+			if (!isset($_POST['verf']) || !verify_csrf_code("sellitem_{$_GET['ID']}", stripslashes($_POST['verf'])))
+			{
+				alert('danger',"{$lang["CSRF_ERROR_TITLE"]}","{$lang["CSRF_ERROR_TEXT"]}");
+				die($h->endpage());
+			}
+			if ($_POST['qty'] > $r['inv_qty'])
+			{
+				alert('danger',"{$lang['ERROR_INVALID']}","{$lang['ITEM_SELL_BAD_QTY']}");
+			}
+			else
+			{
+				$price = $r['itmsellprice'] * $_POST['qty'];
+				item_remove($userid, $r['itmid'], $_POST['qty']);
+				$db->query(
+						"UPDATE `users`
+						 SET `primary_currency` = `primary_currency` + {$price}
+						 WHERE `userid` = $userid");
+				$priceh = number_format($price);
+				alert('success',"{$lang['ERROR_SUCCESS']}","{$lang['ITEM_SELL_SUCCESS1']} {$_POST['qty']} {$r['itmname']}{$lang['ITEM_SELL_SUCCESS2']} {$priceh} {$lang['INDEX_PRIMCURR']}.");
+				$is_log =  $db->escape("{$ir['username']} sold {$_POST['qty']} {$r['itmname']}(s) for {$priceh}");
+				$api->SystemLogsAdd($userid,'itemsell',$is_log);
+			}
+		}
+		$db->free_result($id);
+	}
+	else if (!empty($_GET['ID']) && empty($_POST['qty']))
+	{
+		$id =
+				$db->query(
+						"SELECT `inv_qty`, `itmname`
+						 FROM `inventory` AS `iv`
+						 INNER JOIN `items` AS `it`
+						 ON `iv`.`inv_itemid` = `it`.`itmid`
+						 WHERE `iv`.`inv_id` = {$_GET['ID']}
+						 AND `iv`.`inv_userid` = {$userid}
+						 LIMIT 1");
+		if ($db->num_rows($id) == 0)
+		{
+			alert('danger',"{$lang['ITEM_SELL_ERROR1_TITLE']}","{$lang['ITEM_SELL_ERROR1']}");
+		}
+		else
+		{
+			$r = $db->fetch_row($id);
+			$code = request_csrf_code("sellitem_{$_GET['ID']}");
+			echo "
+			<b>{$lang['ITEM_SELL_FORM1']} {$r['itmname']}(s) {$lang['ITEM_SELL_FORM2']} {$r['inv_qty']} {$lang['ITEM_SELL_FORM3']}</b>
+			<br />
+			<form action='?ID={$_GET['ID']}' method='post'>
+				<table class='table table-bordered'>
+					<tr>
+						<th>
+							{$lang['STAFF_ITEM_GIVE_FORM_QTY']}
+						</th>
+						<td>
+							<input type='text' class='form-control' name='qty' value='{$r['inv_qty']}' />
+						</td>
+					</tr>
+					<tr>
+						<td colspan='2'>
+							<input type='submit' class='btn btn-default' value='{$lang['ITEM_SELL_BTN']}' />
+						</td>
+					</tr>
+					<input type='hidden' name='verf' value='{$code}' />
+				</table>
+			</form>
+			";
+		}
+		$db->free_result($id);
+	}
+	else
+	{
+		alert('danger',"{$lang['ERROR_GENERIC']}","{$lang['GEN_IUOF']}");
+	}
 }
 $h->endpage();
