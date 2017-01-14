4b99cb9bcb4201f5492e82ae56d4e53d2230f5c0
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2017-01-14 18:04:02 -0500

    Redirect back to the origin page.

--- upload/macro.php
+++ upload/macro.php
@@ -4,17 +4,18 @@ require_once('globals.php');
 if(isset($_POST['g-recaptcha-response']))
 {
 	$captcha=$_POST['g-recaptcha-response'];
+	$page = stripslashes(strip_tags($_POST['page']));
 	if(!$captcha)
 	{
 		echo '<h2>Please check the the captcha form.</h2>';
-		header("Location: index.php");
+		header("Location: {$page}");
 		exit;
 	}
 	$response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$set['reCaptcha_private']}&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true); 
 	if($response['success'] == false) 
 	{
 		echo '<h2>You failed the captcha!</h2>';
-		header("Location: index.php");
+		header("Location: {$page}");
 		$api->SystemLogsAdd($userid,'verify',"Verified unsuccessfully.");
 		exit;
 	} 
@@ -23,6 +24,6 @@ if(isset($_POST['g-recaptcha-response']))
 		$time=time();
 		$db->query("UPDATE users SET `last_verified`={$time}, `need_verify` = 0 WHERE userid={$userid}");
 		$api->SystemLogsAdd($userid,'verify',"Verified successfully.");
-		header("Location: index.php");
+		header("Location: {$page}");
 	}
 }
