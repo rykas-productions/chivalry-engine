381c25060494f644219ef298eea956cc65a473db
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-02-08 22:16:35 -0500

    Start work on build 0.0.36

--- upload/installer.php
+++ upload/installer.php
@@ -3,8 +3,8 @@ if (file_exists('./installer.lock'))
 {
     exit;
 }
-$Version=('0.0.35');
-$Build=('0035');
+$Version=('0.0.36');
+$Build=('0036');
 define('MONO_ON', 1);
 session_name('CENGINE');
 session_start();
