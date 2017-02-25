5678b496de48aecd33d09017da77695dd61854d1
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-02-25 14:43:03 -0500

    Removed course days query.

--- upload/crons/day.php
+++ upload/crons/day.php
@@ -17,7 +17,6 @@ if ($db->num_rows($ready_to_run))
 	
 	$db->query("UPDATE users SET `vip_days`=`vip_days`-1 WHERE `vip_days` > 0");
 	$db->query("UPDATE users SET `bank`=`bank`+(`bank`/50) WHERE `bank`>0");
-	$db->query("UPDATE `users` SET `days_left`=`days_left`-1 WHERE `days_left` > 0");
 	
 	$time = 86400;
 	$db->query("UPDATE `crons` SET `nextUpdate`=`nextUpdate`+{$time} WHERE `file`='{$file}'");
