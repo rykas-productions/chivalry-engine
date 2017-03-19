c19bdca3230c8781405d16bc74eb09756522ec7d
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-03-18 22:29:47 -0400

    Build 0.0.41!

--- upload/installer.php
+++ upload/installer.php
@@ -3,8 +3,8 @@ if (file_exists('./installer.lock'))
 {
     exit;
 }
-$Version=('0.0.40');
-$Build=('0040');
+$Version=('0.0.41');
+$Build=('0041');
 define('MONO_ON', 1);
 session_name('CENGINE');
 session_start();
