67ff8122927b9551b3fd36eb30825bc24a3af485
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-04-17 23:43:21 -0400

    Wrong time oops

--- upload/crons/fivemins.php
+++ upload/crons/fivemins.php
@@ -34,7 +34,7 @@ if ($db->num_rows($ready_to_run))
 	$db->query("UPDATE mining SET miningpower=miningpower+(max_miningpower/(10)) WHERE miningpower<max_miningpower");
 	$db->query("UPDATE mining SET miningpower=max_miningpower WHERE miningpower>max_miningpower");
 	
-	$time = 1;
+	$time = 300;
 	$db->query("UPDATE `crons` SET `nextUpdate`=`nextUpdate`+{$time} WHERE `file`='{$file}'");
 }
 
