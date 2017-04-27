a516a443e812fd867423f4d0fcc2cc0ce773ddc2
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-04-27 17:37:26 -0400

    Cleaned up more.

--- upload/login.php
+++ upload/login.php
@@ -33,48 +33,37 @@ if ($set['HTTPS_Support'] == 'true')
 	<meta property="og:image" content="http://vignette1.wikia.nocookie.net/helmet-heroes/images/e/e0/Knight_Helmet.png/revision/latest?cb=20131008030002" />
 	<link rel="shortcut icon" href="http://vignette1.wikia.nocookie.net/helmet-heroes/images/e/e0/Knight_Helmet.png/revision/latest?cb=20131008030002" type="image/x-icon" />
 	<meta name="author" content="<?php echo $set['WebsiteOwner']; ?>">
-
     <?php echo "<title>{$set['WebsiteName']}</title>"; ?>
-
-    <!-- CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<meta name="theme-color" content="#e7e7e7">
 	<link href="css/bs2.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.csss">
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
-    <!-- Custom CSS -->
     <style>
-    body {
+    body 
+	{
         padding-top: 70px;
 		font-size: 16px;
-        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
     }
     </style>
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
-
 <body>
-
-    <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php"><?php echo $set['WebsiteName'] ?></a>
+                <a class="navbar-brand" href="index.php">Chivalry Engine</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -87,44 +76,42 @@ if ($set['HTTPS_Support'] == 'true')
                     </li>
                 </ul>
 				<ul class="nav navbar-nav navbar-right">
-        <li><p class="navbar-text"><?php echo"{$lang['LOGIN_AHA']}"; ?></p></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b> <span class="glyphicon glyphicon-log-in"></span> <?php echo"{$lang['LOGIN_LOGIN']}"; ?></b> <span class="caret"></span></a>
-			<ul id="login-dp" class="dropdown-menu">
-				<li>
-					 <div class="row">
-							<div class="col-md-12">
-								 <?php echo"{$lang['LOGIN_LWE']}"; ?>
-								 <form class="form" role="form" method="post" action="authenticate.php" accept-charset="UTF-8" id="login-nav">
-										<div class="form-group">
-											 <label class="sr-only" for="exampleInputEmail2"><?php echo"{$lang['LOGIN_EMAIL']}"; ?></label>
-											 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="<?php echo"{$lang['LOGIN_EMAIL']}"; ?>" name="email" required>
+					<li>
+						<p class="navbar-text"><?php echo"{$lang['LOGIN_AHA']}"; ?></p>
+					</li>
+					<li class="dropdown">
+					  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b> <span class="glyphicon glyphicon-log-in"></span> <?php echo"{$lang['LOGIN_LOGIN']}"; ?></b> <span class="caret"></span></a>
+						<ul id="login-dp" class="dropdown-menu">
+							<li>
+								 <div class="row">
+										<div class="col-md-12">
+											<?php echo"{$lang['LOGIN_LWE']}"; ?>
+											 <form class="form" role="form" method="post" action="authenticate.php" accept-charset="UTF-8" id="login-nav">
+													<div class="form-group">
+														 <label class="sr-only" for="exampleInputEmail2"><?php echo"{$lang['LOGIN_EMAIL']}"; ?></label>
+														 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="<?php echo"{$lang['LOGIN_EMAIL']}"; ?>" name="email" required>
+													</div>
+													<div class="form-group">
+														 <label class="sr-only" for="exampleInputPassword2"><?php echo"{$lang['LOGIN_PASSWORD']}"; ?></label>
+														 <input type="password" class="form-control" id="exampleInputPassword2" name="password" placeholder="<?php echo"{$lang['LOGIN_PASSWORD']}"; ?>" required>
+													</div>
+													<?php echo"<input type='hidden' name='page' value='{$cpage}'>"; ?>
+													<div class="form-group">
+														 <button type="submit" class="btn btn-primary btn-block"><?php echo"{$lang['LOGIN_SIGNIN']}"; ?></button>
+													</div>
+											 </form>
 										</div>
-										<div class="form-group">
-											 <label class="sr-only" for="exampleInputPassword2"><?php echo"{$lang['LOGIN_PASSWORD']}"; ?></label>
-											 <input type="password" class="form-control" id="exampleInputPassword2" name="password" placeholder="<?php echo"{$lang['LOGIN_PASSWORD']}"; ?>" required>
+										<div class="bottom text-center">
+											<?php echo"{$lang['LOGIN_NH']}"; ?>
 										</div>
-										<?php echo"<input type='hidden' name='page' value='{$cpage}'>"; ?>
-										<div class="form-group">
-											 <button type="submit" class="btn btn-primary btn-block"><?php echo"{$lang['LOGIN_SIGNIN']}"; ?></button><br />
-										</div>
-								 </form>
-							</div>
-							<div class="bottom text-center">
-								<?php echo"{$lang['LOGIN_NH']}"; ?>
-							</div>
-					 </div>
-				</li>
-			</ul>
-        </li>
-      </ul>
+								 </div>
+							</li>
+						</ul>
+					</li>
+				</ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>
-
-    <!-- Page Content -->
     <div class="container">
 		<noscript>
 			<?php
