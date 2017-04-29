2ac99c68469c95bd8af29ff773c74b321bbc80cf
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-04-29 17:35:10 -0400

    Select count... not sum

--- upload/explore.php
+++ upload/explore.php
@@ -9,8 +9,8 @@
 require("globals.php");
 $tresder = (Random(100, 999));
 $time=time();
-$dung_count=$db->fetch_single($db->query("SELECT SUM(`dungeon_user`) FROM `dungeon` WHERE `dungeon_out` > {$time}"));
-$infirm_count=$db->fetch_single($db->query("SELECT SUM(`infirmary_user`) FROM `infirmary` WHERE `infirmary_out` > {$time}"));
+$dung_count=$db->fetch_single($db->query("SELECT COUNT(`dungeon_user`) FROM `dungeon` WHERE `dungeon_out` > {$time}"));
+$infirm_count=$db->fetch_single($db->query("SELECT COUNT(`infirmary_user`) FROM `infirmary` WHERE `infirmary_out` > {$time}"));
 if (empty($dung_count))
 {
 	$dung_count=0;
