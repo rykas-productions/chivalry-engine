2d0e1ff09b44e6abcf07cb6c97f7f746ec4193ec
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-05-30 19:25:09 -0400

    Condense queries

--- upload/crons/fivemins.php
+++ upload/crons/fivemins.php
@@ -12,27 +12,17 @@ $file = 'crons/fivemins.php';
 $ready_to_run = $db->query("SELECT `nextUpdate` FROM `crons` WHERE `file`='{$file}' AND `nextUpdate` <= unix_timestamp()");
 //Place your queries inside this conditional
 if ($db->num_rows($ready_to_run)) 
-{
-	//Brave update
-	$db->query("UPDATE users SET brave=brave+((maxbrave/10)+0.5) WHERE brave<maxbrave");
-	$db->query("UPDATE users SET brave=maxbrave WHERE brave>maxbrave");
-	
-	//HP Update
-	$db->query("UPDATE users SET hp=hp+(maxhp/3) WHERE hp<maxhp");
-	$db->query("UPDATE users SET hp=maxhp WHERE hp>maxhp");
-	
-	//energy update
-	$db->query("UPDATE users SET energy=energy+(maxenergy/(12.5)) WHERE energy<maxenergy AND `vip_days`=0");
-	$db->query("UPDATE users SET energy=energy+(maxenergy/(6)) WHERE energy<maxenergy AND `vip_days`>0");
-	$db->query("UPDATE users SET energy=maxenergy WHERE energy>maxenergy");
-	
-	//Will Update
-	$db->query("UPDATE users SET will=will+(maxwill/(10)) WHERE will<maxwill");
-	$db->query("UPDATE users SET will=maxwill WHERE will>maxwill");
-	
+{	
 	//Mining refill
 	$db->query("UPDATE mining SET miningpower=miningpower+(max_miningpower/(10)) WHERE miningpower<max_miningpower");
 	$db->query("UPDATE mining SET miningpower=max_miningpower WHERE miningpower>max_miningpower");
+	
+	$allusers_query ="UPDATE `users` SET `brave` = LEAST(`brave` + ((`maxbrave` / 10) + 0.5), `maxbrave`),
+        `hp` = LEAST(`hp` + (`maxhp` / 3), `maxhp`), `will` = LEAST(`will` + 10, `maxwill`),
+        `energy` = IF(`vip_days` > 0,
+                   LEAST(`energy` + (`maxenergy` / 6), `maxenergy`),
+                   LEAST(`energy` + (`maxenergy` / 12.5), `maxenergy`))";
+	$db->query($allusers_query);
 	
 	$time = 300;
 	$db->query("UPDATE `crons` SET `nextUpdate`=`nextUpdate`+{$time} WHERE `file`='{$file}'");
