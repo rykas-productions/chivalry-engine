2b67b5f961c4b36218aabd0ae7226b70ae9cc715
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-08-21 20:52:30 -0400

    Simplify the error handler

--- upload/lib/basic_error_handler.php
+++ upload/lib/basic_error_handler.php
@@ -12,49 +12,7 @@ define('DEBUG', true);
 function error_critical($human_error, $debug_error, $action, $context = array())
 {
 	global $userid,$domain,$set;
-    ob_get_clean();
-    header('HTTP/1.1 500 Internal Server Error');
-	?>
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-	<center>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-	<meta name="theme-color" content="#000000">
-
-    <?php echo "<title>{$set['WebsiteName']} - Critical Error</title>"; ?>
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link href="css/bs2.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
-    <!-- Custom CSS -->
-    <style>
-    body {
-        padding-top: 70px;
-        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
-    }
-	a {
-		color: gray;
-	}
-    </style>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-	
-	<?php
+    echo "<title>{$set['WebsiteName']} - Critical Error</title>";
     if (isset($set) && is_array($set) && array_key_exists('WebsiteName', $set))
     {
         echo "<h1>{$set['WebsiteName']} - Critical Error</h1>";
