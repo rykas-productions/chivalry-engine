476ea993c3afecd8bc2a9334a9855ae3cbc56720
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-09 14:18:13 -0400

    Load the full, minified jQuery

--- upload/header.php
+++ upload/header.php
@@ -290,7 +290,7 @@ class headers
         <link rel="stylesheet" href="css/bs2.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- jQuery Version 3.2.1 -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
         <!-- Bootstrap Core JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
