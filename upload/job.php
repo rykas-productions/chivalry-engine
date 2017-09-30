d67abab8a9dda9ab63045ddd381b7dc844200b2c
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-29 20:37:38 -0400

    Job workings!

---
+++ upload/job.php
@@ -0,0 +1,234 @@
+<?php
+/*
+	File:		job.php
+	Created: 	9/29/2018 at 12:17AM Eastern Time
+	Info: 		The main game job page.
+	Author:		TheMasterGeneral
+	Website: 	https://github.com/MasterGeneral156/chivalry-engine
+*/
+$jobquery = 1;
+require('globals.php');
+$_GET['interview'] = (isset($_GET['interview']) && is_numeric($_GET['interview'])) ? abs(intval($_GET['interview'])) : 0;
+if (empty($ir['job'])) {
+    if (empty($_GET['interview'])) {
+        echo "It appears you are unemployed. A list of businesses hiring are listed below.<br />";
+        $q = $db->query("SELECT * FROM `jobs`");
+        echo "<table class='table table-bordered'>
+        <tr>
+            <th>
+                Business Name
+            </th>
+            <th>
+                Business Owner
+            </th>
+            <th>
+                Business Description
+            </th>
+            <th>
+
+            </th>
+        </tr>";
+        while ($r = $db->fetch_row($q)) {
+            echo "<tr>
+                <td>
+                    {$r['jNAME']}
+                </td>
+                <td>
+                    {$r['jBOSS']}
+                </td>
+                <td>
+                    {$r['jDESC']}
+                </td>
+                <td>
+                    <a href='?interview={$r['jRANK']}'>Attend Interview</a>
+                </td>
+            </tr>";
+        }
+        echo "</table>";
+        $db->free_result($q);
+    } else {
+        $q = $db->query("SELECT `j`.*, `jr`.*
+                        FROM `jobs` AS `j`
+                        INNER JOIN `job_ranks` AS `jr`
+                        ON `j`.`jSTART` = `jr`.`jrID`
+                        WHERE `j`.`jRANK` = {$_GET['interview']}");
+        if ($db->num_rows($q) == 0) {
+            $db->free_result($q);
+            alert("danger", "Uh Oh!", "You are trying to attempt the interview for a job that isn't hiring.", true, 'job.php');
+            die($h->endpage());
+        }
+        $r = $db->fetch_row($q);
+        $db->free_result($q);
+        echo "<b>{$r['jBOSS']}:</b> So, {$ir['username']}, it appears you are wanting to work with our company. Can you tell me a little bit about yourself?<br />
+        <b>{$ir['username']}:</b> Of course! I'm level {$ir['level']}, have {$ir['strength']} strength, {$ir['iq']} IQ, and {$ir['labor']} labor. I hope these skills are useful to the company.<br />";
+        if ($ir['strength'] >= $r['jrSTR'] && $ir['labor'] >= $r['jrLAB'] && $ir['iq'] >= $r['jrIQ']) {
+            $db->query("UPDATE `users`
+                        SET `job` = {$_GET['interview']},
+                        `jobrank` = {$r['jrID']}
+                        WHERE `userid` = {$userid}");
+            echo "<b>{$r['jBOSS']}:</b> It appears you fit our basic requirements. Is starting at {$r['jrPRIMPAY']} Primary Currency and/or {$r['jrSECONDARY']} Secondary Currency per hour fine with you?<br />
+            <b>{$ir['username']}:</b> Yes it is!<br />
+            <b>{$r['jBOSS']}:</b> Alright, well, get to work then! Welcome aboard!<br />
+            <a href='job.php'>Get to Work</a>";
+        } else {
+            echo "<b>{$r['jBOSS']}:</b> We apologize, {$ir['username']}, but you do not have the necessary requirements needed to join our company. You'll need";
+            if ($ir['strength'] < $r['jrSTR']) {
+                $s = $r['jrSTR'] - $ir['strength'];
+                echo " $s more strength, ";
+            }
+            if ($ir['labor'] < $r['jrLAB']) {
+                $s = $r['jrLAB'] - $ir['labor'];
+                echo " $s more labor, ";
+            }
+            if ($ir['iq'] < $r['jrIQ']) {
+                $s = $r['jrIQ'] - $ir['iq'];
+                echo " $s more IQ, ";
+            }
+            echo "before you'll be able to work here with our company.
+			<br />
+			&gt; <a href='index.php'>Go Home, Crying</a>";
+        }
+    }
+    $h->endpage();
+} else {
+    if (!isset($_GET['action'])) {
+        $_GET['action'] = '';
+    }
+    switch ($_GET['action']) {
+        case 'quit':
+            job_quit();
+            break;
+        case 'promote':
+            job_promote();
+            break;
+        case 'work':
+            job_work();
+            break;
+        default:
+            job_index();
+            break;
+    }
+}
+function job_index()
+{
+    global $db, $ir, $h;
+    echo "<h3>Your Job</h3>
+    You currently work in the {$ir['jNAME']}! You receive {$ir['jrPRIMPAY']} Primary Currency and/or
+    {$ir['jrSECONDARY']} Secondary Currency each hour you work as you're required. You've worked {$ir['jobwork']} /
+    {$ir['jrACT']} times this hour.
+    <table class='table table-bordered'>
+    <tr>
+        <th>
+            Title
+        </th>
+        <th>
+            Hourly Wage
+        </th>
+        <th>
+            Requirements
+        </th>
+    </tr>";
+    $q = $db->query("SELECT * FROM `job_ranks` WHERE `jrJOB` = {$ir['job']} ORDER BY (`jrPRIMPAY` + `jrSECONDARY`) ASC");
+    while ($r = $db->fetch_row($q)) {
+        echo "
+        <tr>
+            <td>
+                {$r['jrRANK']}
+            </td>
+            <td>
+                {$r['jrPRIMPAY']} Primary<br />
+                {$r['jrSECONDARY']} Secondary
+            </td>
+            <td>
+                {$r['jrSTR']} Strength<br />
+                {$r['jrLAB']} Labor<br />
+                {$r['jrIQ']} IQ<br />
+                {$r['jrACT']} Work/Hour
+            </td>
+        </tr>";
+    }
+    echo "</table>
+    &gt; <a href='?action=work'>Begin Work</a>
+	<br />
+    &gt; <a href='?action=promote'>Try To Get Promoted</a>
+	<br />
+	&gt; <a href='?action=quit'>Quit Job</a>";
+    $h->endpage();
+}
+
+function job_quit()
+{
+    global $db, $h, $userid;
+    $db->query("UPDATE `users` SET `job` = 0, `jobrank` = 0, `jobwork` = 0 WHERE `userid` = {$userid}");
+    alert('success', "Success!", "You have successfully quit your job.", true, 'job.php');
+    $h->endpage();
+}
+
+function job_promote()
+{
+    global $db, $h, $ir, $userid;
+    $q = $db->query("SELECT *
+                    FROM `job_ranks`
+                    WHERE (`jrPRIMPAY` + `jrSECONDARY`) > ({$ir['jrPRIMPAY']} + {$ir['jrSECONDARY']})
+                    AND `jrSTR` <= {$ir['strength']}
+                    AND `jrLAB` <= {$ir['labor']}
+                    AND `jrIQ` <= {$ir['iq']}
+                    AND `jrJOB` = {$ir['job']}
+                    ORDER BY (`jrPRIMPAY` + `jrSECONDARY`) DESC
+                    LIMIT 1");
+    if ($db->num_rows($q) == 0) {
+        alert("danger", "Uh Oh!", "You cannot be promoted at this time. This might be because you've reached the max rank
+        in your current job, or have not yet to meet the requirements of the next level.", true, 'job.php');
+    } else {
+        $r = $db->fetch_row($q);
+        $db->query("UPDATE `users` SET `jobrank` = {$r['jrID']} WHERE `userid` = {$userid}");
+        alert("success", "Success!", "Congratulations! You have been promoted to {$r['jrNAME']}!", true, 'job.php');
+    }
+    $db->free_result($q);
+    $h->endpage();
+}
+
+function job_work()
+{
+    global $db, $h, $ir, $userid, $api;
+    if (!isset($_GET['dowork'])) {
+        if ($ir['jobwork'] >= $ir['jrACT']) {
+            alert('danger', "Uh Oh!", "You've already worked the maximum times you are required to this hour. You will not
+            gain overtime! Take it easy, bro.", true, 'job.php');
+        } else {
+            echo "You need to work {$ir['jrACT']} times an hour to get paid. You've only worked {$ir['jobwork']} this hour.
+        Each attempt at working will deplete your energy by 10%, and your Will by 5%. Do you wish to work?<br />
+        <a class='btn btn-primary' href='?action=work&dowork=1'>Begin Working</a>
+        ";
+        }
+    } else {
+        $will = ($api->UserInfoGet($userid, 'will', true));
+        $energy = ($api->UserInfoGet($userid, 'energy', true));
+        if ($ir['jobwork'] >= $ir['jrACT']) {
+            alert('danger', "Uh Oh!", "You've already worked the maximum times you are required to this hour. You will not
+            gain overtime! Take it easy, bro.", true, 'job.php');
+            die($h->endpage());
+        }
+        if ($will < 7) {
+            alert("danger", "Uh Oh!", "You need 7% Will to work, you only have {$will}%.", true, '?action=work');
+            die($h->endpage());
+        }
+        if ($energy < 10) {
+            alert("danger", "Uh Oh!", "You need 10% Energy to work, you only have {$energy}%.", true, '?action=work');
+            die($h->endpage());
+        }
+        $WorkUnits = Random(0, 3);
+        if ($WorkUnits == 0) {
+            alert("danger", "Uh Oh!", "You were unable to focus, and thus, you were not able to work as hard as you should have.", false);
+        } else {
+            alert("success", "Success!", "You got to work and knocked out {$WorkUnits} pieces of work. How productive!", false);
+        }
+        $db->query("UPDATE `users` SET `jobwork` = `jobwork` + {$WorkUnits} WHERE `userid` = {$userid}");
+        $api->UserInfoSet($userid, 'will', -7, true);
+        $api->UserInfoSet($userid, 'energy', -10, true);
+        echo "<a class='btn btn-primary' href='?action=work&dowork=1'>Work Again</a><br /><br />
+        <a class='btn btn-primary' href='job.php'>Go Back</a>";
+    }
+    $h->endpage();
+}
+
