88795435ed020b424ab0dfc08e5a3f17810089ee
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-10-04 21:33:05 -0400

    Jobs query...

--- upload/crons/hour.php
+++ upload/crons/hour.php
@@ -14,6 +14,16 @@ $ready_to_run = $db->query("SELECT `nextUpdate` FROM `crons` WHERE `file`='{$fil
 //Place your queries inside the conditional
 if ($db->num_rows($ready_to_run)) {
     $time = 3600;
+
+    //Job crons!
+    $db->query("UPDATE `users` AS `u`
+        LEFT JOIN `job_ranks` as `jr` ON `jr`.`jrID` = `u`.`jobrank`
+        SET `u`.`primary_currency` = `u`.`primary_currency` + `jr`.`jrPRIMPAY`,
+        `u`.`secondary_currency` = `u`.`secondary_currency` + `jr`.`jrSECONDARY`
+        WHERE `u`.`job` > 0 AND `u`.`jobrank` > 0 AND `u`.`jobwork` >= `jr`.`jrACT`");
+    $db->query("UPDATE `users` SET `jobwork` = 0 WHERE `jobwork` > 0 AND `job` > 0 AND `jobrank` > 0");
+
+    //Update queries!
     $db->query("UPDATE `crons` SET `nextUpdate`=`nextUpdate`+{$time} WHERE `file`='{$file}'");
 }
 ?>
