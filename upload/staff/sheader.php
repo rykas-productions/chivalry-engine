4d8e95441b9a7c9c9a8dcb0cf5a920b50da6b842
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-04-27 10:33:40 -0400

    New game header??

--- upload/staff/sheader.php
+++ upload/staff/sheader.php
@@ -67,7 +67,7 @@ class headers
                 <link rel="shortcut icon" href="https://res.cloudinary.com/dydidizue/image/upload/v1520819511/logo-optimized.png" type="image/x-icon"/>
 				<link rel="icon" sizes="192x192" href="https://res.cloudinary.com/dydidizue/image/upload/c_scale,h_192/v1520819749/logo.png">
 				<link rel="icon" sizes="128x128" href="https://res.cloudinary.com/dydidizue/image/upload/c_scale,h_128/v1520819749/logo.png">
-				<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MasterGeneral156/chivalry-is-dead-game-cdn@1/css/game-v1.11.min.css">
+				<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MasterGeneral156/chivalry-is-dead-game-cdn@1.0.9/css/game-v1.11.min.css">
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/MasterGeneral156/chivalry-is-dead-game-cdn@1/css/game-icons.min.css">
 				</head>
         </head>
@@ -98,18 +98,33 @@ class headers
                     <div class="my-2 my-lg-0">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item">
+							<div class="fa-2x">
                                 <a class="nav-link"
-                                   href="../inbox.php"><?php echo "<i
-                                        class='fa fa-fw fa-inbox'></i> Inbox <span class='badge badge-pill badge-primary'>{$ir['mail']}</span>"; ?></a>
+                                   href="../inbox.php">
+									<span class="fa-layers fa-fw">
+										<i class="fas fa-envelope"></i>
+											<span class="fa-layers-counter" style="background:Tomato"><?php echo $ir['mail']; ?></span>
+									</span>
+								</a>
+								</div>
                             </li>
                             <li class="nav-item">
+                                <div class="fa-2x">
                                 <a class="nav-link"
-                                   href="../notifications.php"><?php echo "<i
-                                        class='fas fa-fw fa-bell'></i> Notifications <span class='badge badge-pill badge-primary'>{$ir['notifications']}</span>"; ?></a>
+                                   href="../notifications.php">
+									<span class="fa-layers fa-fw">
+										<i class="fas fa-bell"></i>
+											<span class="fa-layers-counter" style="background:Tomato"><?php echo $ir['notifications']; ?></span>
+									</span>
+								</a>
+								</div>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="../inventory.php"><?php echo "<i
-                                        class='fas fa-fw fa-briefcase'></i> Inventory"; ?></a>
+								<div class="fa-2x">
+                                <a class="nav-link" href="../inventory.php">
+									<i class='fas fa-fw fa-briefcase'></i>
+								</a>
+								</div>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
@@ -117,7 +132,7 @@ class headers
                                     <?php
                                     //User has a display picture, lets show it!
                                     if ($ir['display_pic']) {
-                                        echo "<img src='{$ir['display_pic']}' width='30' height='30'>";
+                                        echo "<img src='{$ir['display_pic']}' width='45' height='45'>";
                                     }
                                     echo " Hello, {$ir['username']}!";
                                     ?>
