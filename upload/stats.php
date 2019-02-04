326602d46aeb54728c166259c39383478737ffa8
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-02-04 18:48:50 -0500

    Previous commit, basically.

--- upload/stats.php
+++ upload/stats.php
@@ -32,43 +32,6 @@ google.charts.load('current', {'packages':['corechart']});
 		  ['Defender',      {$Defender}]
         ]);
 		
-		var data4 = google.visualization.arrayToDataTable([
-          ['Users', 'Amount'],
-          ['Windows 7',     {$Win7}],
-		  ['Android',     {$Android}],
-		  ['Windows Phone',     {$WinPho}],
-		  ['Blackberry',     {$Blackberry}],
-		  ['Mobile',     {$Mobile}],
-		  ['Unknown',     {$UnknownOS}],
-		  ['iPhone',     {$iPhone}],
-		  ['iPod',     {$iPod}],
-		  ['iPad',     {$iPad}],
-		  ['Ubuntu',     {$Ubuntu}],
-		  ['Linux',     {$Linux}],
-		  ['Mac OS 9',     {$OS9}],
-		  ['Windows Vista',     {$WinV}],
-		  ['Windows XP',     {$WinXP}],
-		  ['Mac OS X',     {$OSX}],
-		  ['Windows 8.1',     {$Win81}],
-		  ['Windows 10',     {$Win10}],
-          ['Windows 8',     {$Win8}]
-        ]);
-		
-		var data5 = google.visualization.arrayToDataTable([
-          ['Users', 'Amount'],
-          ['Chrome',     {$Chrome}],
-		  ['Internet Explorer',     {$IE}],
-		  ['Safari',     {$Safari}],
-		  ['Edge',     {$Edge}],
-		  ['Opera',     {$Opera}],
-		  ['Netscape',     {$NS}],
-		  ['Maxthon',     {$Maxthon}],
-		  ['Konqueror',     {$Konqueror}],
-		  ['Mobile Browser',     {$MobileBro}],
-		  ['Unknown',     {$UnknownBro}],
-		  ['Mobile App', {$App}],
-          ['Firefox',     {$FF}]
-        ]);
 		var options2 = {
           title: 'Gender Ratio'
         };
@@ -77,24 +40,12 @@ google.charts.load('current', {'packages':['corechart']});
           title: 'Class Ratio',
 		  colors: ['#FF0000', '#0000FF', '#00FF00']
         };
-		
-		var options4 = {
-          title: 'User Operating Systems'
-        };
-		
-		var options5 = {
-          title: 'User Browser Choice'
-        };
 
 		var chart2 = new google.visualization.PieChart(document.getElementById('gender'));
 		var chart3 = new google.visualization.PieChart(document.getElementById('class'));
-		var chart4 = new google.visualization.PieChart(document.getElementById('os'));
-		var chart5 = new google.visualization.PieChart(document.getElementById('browser'));
 
 		chart2.draw(data2, options2);
 		chart3.draw(data3, options3);
-		chart4.draw(data4, options4);
-		chart5.draw(data5, options5);
       }
 </script>
 	<table width='100%' class='table table-bordered'>
@@ -104,14 +55,6 @@ google.charts.load('current', {'packages':['corechart']});
 			</td>
 			<td width='50%'>
 				<div id='class'></div>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<div id='browser'></div>
-			</td>
-			<td>
-				<div id='os'></div>
 			</td>
 		</tr>
 	</table>
