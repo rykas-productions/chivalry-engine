2638524c55cf0976d2519d0c0bdfc1c7a453841a
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-04-25 17:19:21 -0400

    Delete unneeded JS.

--- upload/js/completion-min.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("#completion").dialog({
-        modal: !0, dialogClass: "no-close", buttons: {
-            Close: function () {
-                $(this).dialog("close"), $(location).prop("href", "?action=menu")
-            }
-        }
-    })
-});
--- upload/js/register-min.js
+++
@@ -1,7 +0,0 @@
-function CheckPasswords(password){$.ajax({type:"POST",url:"js/script/check.php",data:"password="+escape(password),success:function(resps){$("#passwordresult").html(resps)}})}
-function goBack(){window.history.back()}
-function CheckUsername(name){$.ajax({type:"POST",url:"js/script/checkun.php",data:"username="+escape(name),success:function(resps){$("#usernameresult").html(resps)}})}
-function OutputTeam(team){var value=team.value;$.ajax({type:"POST",url:"js/script/outputteam.php",data:"team="+escape(value),success:function(resps){$("#teamresult").html(resps)}})}
-function CheckEmail(email){$.ajax({type:"POST",url:"js/script/checkem.php",data:"email="+escape(email),success:function(resps){$("#emailresult").html(resps)}})}
-function PasswordMatch(){pwt1=$("#password").val();pwt2=$("#cpassword").val();if(pwt1==pwt2){document.getElementById('password').className='form-control is-valid';document.getElementById('cpassword').className='form-control is-valid';document.getElementById("cpasswordresult").innerHTML=''}
-else{document.getElementById('password').className='form-control is-invalid';document.getElementById('cpassword').className='form-control is-invalid';document.getElementById("cpasswordresult").innerHTML='Passwords do not match.'}}
