7cc12ccd232e3fec36bca5a10e703db0cf059557
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-07-12 17:03:42 -0400

    Account class. Used for account based stuff... placeholder functions.

---
+++ upload/class/class_account.php
@@ -0,0 +1,52 @@
+<?php
+if (!defined('MONO_ON')) {
+    exit;
+}
+
+class account
+{
+	function createAccount()
+	{
+		
+	}
+	function authAccount()
+	{
+		
+	}
+	function deleteAccount()
+	{
+		
+	}
+	function updateAccount()
+	{
+		
+	}
+	function pageLoadUpdateAccount()
+	{
+		
+	}
+	function deauthAccount()
+	{
+		
+	}
+	function generateCSRF()
+	{
+		
+	}
+	function generateFormCSRF()
+	{
+		
+	}
+	function checkCSRF()
+	{
+		
+	}
+	function invalidateCSRF()
+	{
+		
+	}
+	function resetAccount()
+	{
+		
+	}
+}
