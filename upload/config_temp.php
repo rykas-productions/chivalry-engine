94c79bb9182eb797f5e5ea3f65b4f91ab0fc36e7
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-09-29 19:45:30 -0400

    Temp file.

---
+++ upload/config_temp.php
@@ -0,0 +1,17 @@
+<?php
+//Rename to config.php when you fill this out completely.
+define('db_host','');	//127.0.0.1, localhost, other IP
+define('db_username','');	//Database username
+define('db_password','');	//database password
+define('db_database','');	//database table
+define('db_driver','mysqli');		//mysqli or pdo
+define('game_key','');		//game key
+define('primary_currency','Primary Currency');
+define('secondary_currency','Secondary Currency');
+define('stat_strength','Strength');
+define('stat_agility','Agility');
+define('stat_labor','Labor');
+define('stat_guard','Guard');
+define('stat_iq','IQ');
+define('itemeffects',3);
+define('sessionTimeoutSeconds',1800);
