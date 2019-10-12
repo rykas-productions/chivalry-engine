bbe4fedf8b2757184e8d1723e583b639884fc701
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-10-12 13:55:08 -0400

    Changed to Darkly as engine base.

--- upload/headers_auth.php
+++ upload/headers_auth.php
@@ -63,7 +63,7 @@ class headers
 		global $set;
 		echo '
 		<header>
-			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
 				<a class="navbar-brand" href="index.php">' . $set['gameName'] . '</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
