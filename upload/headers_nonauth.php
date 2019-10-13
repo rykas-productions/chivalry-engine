af07e8d94d0e5f8489bc159dfa1ff82a37350486
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-10-13 17:59:00 -0400

    Change to United Bootstrap CSS oof

--- upload/headers_nonauth.php
+++ upload/headers_nonauth.php
@@ -63,7 +63,7 @@ class headers
 		global $set;
 		echo '
 		<header>
-			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
 				<a class="navbar-brand" href="login.php">' . $set['gameName'] . '</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
