d96a6f3e165b643bba00a10f31ba1b6e92eb6b79
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-06-14 02:50:42 -0400

    2.0.2

--- upload/installer.php
+++ upload/installer.php
@@ -31,8 +31,8 @@ if (file_exists('./installer.lock'))
 {
     exit;
 }
-$Version=('2.0.1');
-$Build=('2.0.1');
+$Version=('2.0.2');
+$Build=('2.0.2');
 define('MONO_ON', 1);
 session_name('CEV2');
 session_start();
