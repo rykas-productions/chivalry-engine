0aaf8eeaa09af061cfaf750e4e319c2c31ed2661
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-06-21 15:58:47 -0400

    Initial bootstrap 5 port

--- upload/headers_nonauth.php
+++ upload/headers_nonauth.php
@@ -44,7 +44,7 @@ class headers
 	
 	function loadCSS()
 	{
-		echo '<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+		echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
 		<style type="text/css">
 			main > .container 
 			{
@@ -55,9 +55,9 @@ class headers
 	
 	function loadJS()
 	{
-		echo '<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-		<script src="assets/js/bootstrap.js"></script>
+		echo '
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 		</head>';
 	}
 	function loadTopMenu()
