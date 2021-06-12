517ad332bfbc7d18e9e41c404278eca0af8b2b5e
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-06-12 16:41:18 -0400

    QoL fix

--- upload/smelt.php
+++ upload/smelt.php
@@ -60,7 +60,7 @@ function home()
 			echo "
 			<tr>
 				<td>
-					{$output_item} x " . number_format($r['smelt_qty_output']) . "
+					<a href='iteminfo.php?ID={$r['smelt_output']}'>{$output_item}</a> x " . number_format($r['smelt_qty_output']) . "
 				</td>
 				<td>";
 			$n = 0;
@@ -73,7 +73,7 @@ function home()
 					$t['itmname'] = "<span class='text-danger'>" . $t['itmname'] . "";
 					$can_craft = FALSE;
 				}
-				$items_needed .= $t['itmname'] . " x " . number_format($qty[$n]) . " (Have " . number_format($api->UserCountItem($userid, $i)) . ")</span><br />";
+				$items_needed .= "<a href='iteminfo.php?ID={$i}'>" .$t['itmname'] . "</a> x " . number_format($qty[$n]) . " (Have " . number_format($api->UserCountItem($userid, $i)) . ")</span><br />";
 				$n++;
 			}
 			unset($n);
