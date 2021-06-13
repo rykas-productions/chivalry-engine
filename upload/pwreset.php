48949303247bfd85306368543c638f5793ac7a41
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-06-13 15:49:17 -0400

    Some changes here.

--- upload/pwreset.php
+++ upload/pwreset.php
@@ -9,6 +9,7 @@
 */
 require('globals_nonauth.php');
 $from = $set['sending_email'];
+
 if (!isset($_GET['step'])) {
     $_GET['step'] = '';
 }
@@ -23,6 +24,16 @@ switch ($_GET['step']) {
 function one()
 {
     global $db, $from, $set, $api;
+    require('lib/bbcode_engine.php');
+    $AnnouncementQuery = $db->query("/*qc=on*/SELECT `ann_text`,`ann_time` FROM `announcements` ORDER BY `ann_time` desc LIMIT 1");
+    $ANN = $db->fetch_row($AnnouncementQuery);
+    $ANN['ann_text']=substr($ANN['ann_text'], 0, 330);
+    //$parser->parse($ANN['ann_text']);
+    $last24hr=time()-86400;
+    $totalplayers=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users`"));
+    $playersonline=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `laston` > {$last24hr}"));
+    $signups=$db->fetch_single($db->query("SELECT COUNT(`userid`) FROM `users` WHERE `registertime` > {$last24hr}"));
+    
     if (isset($_POST['email'])) {
         if (!isset($_POST['email']) || !valid_email(stripslashes($_POST['email']))) {
             alert('danger', "Uh Oh!", "You input an invalid email address.", false);
@@ -57,13 +68,117 @@ function one()
         alert('success', "Success!", "If there is an account associated to the email address you input, you will be
 		    emailed with steps on how to start the password reset process.", false);
     } else {
-        alert('info', "Information!", "Please enter the email adress tied to your account so we can send information on how to reset your password. Please be sure to check your junk folder.", false);
+        echo "<div class='row'>
+        <div class='col-md-6 col-lg-5 col-xl-4 col-xxl-3'>
+        <div class='card'>
+        <div class='card-header'>
+        <div class='row'>
+        <div class='col'>
+        Sign In
+        </div>
+        <div class='col'>
+        <a href='login.php'>Sign In!</a>
+        </div>
+        </div>
+        </div>
+        <div class='card-body'>";
+        alert('info', "", "Please enter the email adress tied to your account so we can send information on how to reset your password. Please be sure to check your junk folder.", false);
         echo "
-		<form method='post'>
-			<input type='email' name='email' required='1' class='form-control'>
-			<br />
-			<input type='submit' class='btn btn-primary'>
-		</form>";
+        <form method='post'>
+        <input type='email' name='email' class='form-control' required='true' placeholder='Your email address'><br />
+        <input type='submit' class='btn btn-primary btn-block' value='Recover Password'><br />
+        New here? <a href='register.php'>Sign up</a> for an account!
+        </form>
+        </div>
+            </div>
+            <br />
+            </div>
+            <div class='col-md-6 col-lg-7 col-xl-8 col-xxl-4'>
+            <div class='card'>
+            <div class='card-header'>
+            Warrior with no empathy
+            </div>
+            <div class='card-body'>
+            {$set['Website_Description']}
+            </div>
+            </div>
+            </div>
+            <br />
+            <div class='col-md-6 col-lg-4 col-xxl-5'>
+            <div class='card'>
+            <div class='card-header'>
+            Highest Ranked Players
+            </div>
+            <div class='card-body'>";
+                $Rank = 0;
+                $RankPlayerQuery =
+                    $db->query("SELECT u.`userid`, `level`, `username`,
+                    `strength`, `agility`, `guard`, `labor`, `IQ`
+                    FROM `users` AS `u`
+                    INNER JOIN `userstats` AS `us`
+                    ON `u`.`userid` = `us`.`userid`
+                    WHERE `u`.`user_level` != 'Admin' AND `u`.`user_level` != 'NPC'
+                        ORDER BY (`strength` + `agility` + `guard` + `labor` + `IQ`)
+                        DESC, `u`.`userid` ASC
+                        LIMIT 10");
+                while ($pdata = $db->fetch_row($RankPlayerQuery)) {
+                    $Rank = $Rank + 1;
+                    echo "<div class='row'>
+                    <div class='col col-md-2'>
+                    {$Rank}
+                    </div>
+                    <div class='col'>
+                    " . parseUsername($pdata['userid']) . " [{$pdata['userid']}]
+                    </div>
+                    <div class='col col-4'>
+                    Level " . number_format($pdata['level']) . "
+                        </div>
+                        </div>";
+                }
+                echo"
+                        </div>
+                        </div>
+                        <br />
+                        </div>
+                        <div class='col-md-6 col-lg-7 col-xl-8 col-xxl-4'>
+                        <div class='card'>
+                        <div class='card-header'>
+                        Gameplay
+                        </div>
+                        <div class='card-body'>
+                        Players must get stronger in order to defeat those who oppose them. How they get there is entirely up to them!
+                        Players may commit crimes and become a crimelord, or hit the training grounds to become a well rounded warrior.
+                        Estates are available purchase to accomodate yourself growing as the warrior they claim to be. <br />
+                        Other warriors will attempt you from reaching the number one warrior spot... but are you really going to be stopped?
+                        </div>
+                        </div>
+                        </div>
+                        <br />
+                        <div class='col-md-6 col-lg-5 col-xl-4 col-xxl-3'>
+                        <div class='card'>
+                        <div class='card-header'>
+                        No Installation Required!
+                        </div>
+                        <div class='card-body'>
+                        " . number_format($playersonline) . " Players Online Today<br />
+                        " . number_format($totalplayers) . " Total Players<br />
+                        " . number_format($signups) . " New Players Today<br />
+                        Most Users Online: " . number_format($set['mostUsersOn']) . " Users<br />" . DateTime_Parse($set['mostUsersOnTime']) . "
+                            </div>
+                            </div>
+                            </div>
+                            <div class='col-md-6 col-lg-7 col-xl-8 col-xxl-5'>
+                            <div class='card'>
+                            <div class='card-header'>
+                            Latest Announcement
+                            </div>
+                            <div class='card-body'>
+                            " . $parser->getAsHtml() . "<br />
+                            <small>" . DateTime_Parse($ANN['ann_time']) . "</small>
+                                </div>
+                                </div>
+                                </div>
+                                </div>";
     }
 }
 
@@ -92,7 +207,7 @@ function two()
             $db->query("UPDATE `user_settings` SET `security_key` = '{$randomizer}' WHERE `userid` = {$newuserid}");
             $db->query("DELETE FROM `pw_recovery` WHERE `pwr_code` = '{$token}'");
             alert('success', "Success!", "Your new password has been emailed to you. If you were previously logged in,
-			    your session has been terminated.", false);
+			    your session has been terminated.", true, 'login.php', 'Login');
         }
     } else {
         alert('danger', "Uh Oh!", "Please specify a recovery token.", false);
