b72cba5247bed81c4ae07e5b822ea23a928be87f
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-06-24 20:57:42 -0400

    Short number parse and more info

--- upload/inventdump.php
+++ upload/inventdump.php
@@ -22,13 +22,13 @@ else
         $totalcost=$totalcost+($r['itmbuyprice']*$r['inv_qty']);
         $db->query("DELETE FROM `inventory` WHERE `inv_id` = {$r['inv_id']}");
     }
-    echo "Done. That's " . number_format($totalcost) . " Copper Coins down the drain.";
+    echo "Done. That's " . shortNumberParse($totalcost) . " Copper Coins down the drain.";
     if ($totalcost > 500000000)
     {
         $achieved=$db->query("/*qc=on*/SELECT * FROM `achievements_done` WHERE `userid` = {$userid} and `achievement` = 85");
         if ($db->num_rows($achieved) == 0)
         {
-            echo "<br />Huh... you've dumped over 500M Copper Coins worth... you deserve the <b>Chump Change</b> achievement.";
+            echo "<br />Huh... you've dumped over 500M Copper Coins worth... you deserve the <b>Chump Change</b> achievement. We've given you a badge and a skill point.";
             $api->UserGiveItem($userid,245,1);
             $db->query("INSERT INTO `achievements_done` (`userid`, `achievement`) VALUES ('{$userid}', '85')");
             $db->query("UPDATE `user_settings` SET `skill_points` = `skill_points` + 1 WHERE `userid` = {$userid}");
