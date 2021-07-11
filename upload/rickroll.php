c21e90a819548406318d14932b017f5dcbe2150c
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-07-11 03:58:57 -0400

    Yep, its a working rickroll.

--- upload/rickroll.php
+++ upload/rickroll.php
@@ -4,10 +4,9 @@ if ($ir['rickroll'] != 0)
     alert('danger',"Rick-Rolled!","You have been rick-rolled by {$api->SystemUserIDtoName($ir['rickroll'])}!!");
     ?>
     <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" id="ytplayer"
-            type="text/html"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&origin=http://chivalryisdeadgame.com" frameborder="0">
-        </iframe>
+        <video class="embed-responsive-item" id="ytplayer" autoplay>
+        	<source src='./assets/video/rick-roll.mp4' type='video/mp4'>
+        </video>
     </div>
     <?php
     $db->query("UPDATE `user_settings` SET `rickroll` = 0 WHERE `userid` = {$userid}");
