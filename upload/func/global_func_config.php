aa387a21f7b8d6ce193c19f51c0f02daba8611ad
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-07-15 22:35:12 -0400

    Module config class from Chivalry Engien V3

---
+++ upload/func/global_func_config.php
@@ -0,0 +1,60 @@
+<?php
+/*	File:		global_func_config.php
+	Created: 	Jul 15, 2021; 10:02:52 PM
+	Info: 		Functions for module configuration.
+	Author:		TheMasterGeneral
+	Website: 	https://chivalryisdeadgame.com/
+*/
+//Back ported from Chivalry Engine V3
+function readConfigFromDB($moduleName)
+{
+    global $db;
+    $q=$db->query("SELECT `setting_value` FROM `settings` WHERE `setting_name` = '{$moduleName}_config'");
+    if ($db->num_rows($q) == 0)
+        return false;
+    else
+        return $db->fetch_single($q);
+            
+}
+//Back ported from Chivalry Engine V3
+function writeConfigToDB($moduleName, $configJson)
+{
+    global $db;
+    $q=$db->query("SELECT `setting_value` FROM `settings` WHERE `setting_name` = '{$moduleName}_config'");
+    if ($db->num_rows($q) == 0)
+        $db->query("INSERT INTO `settings` (`setting_name`, `setting_value`) VALUES ('{$moduleName}_config', '{$configJson}')");
+    else
+        $db->query("UPDATE `settings` SET `setting_value` = '{$configJson}' WHERE `setting_name` = '{$moduleName}_config'");
+}
+/**
+ * @desc Internal function to properly format JSON objects to the string used for the database storage.
+ * @param string $string Config string to encode in JSON.
+ * @return string Config JSON
+ */
+//Back ported from Chivalry Engine V3
+function formatConfig(array $string)
+{
+    return json_encode($string, JSON_FORCE_OBJECT);
+}
+
+/**
+ * @desc Internal function to properly remove JSON formatting.
+ * @param json $json json object to turn into a string.
+ * @return json Config String
+ */
+//Back ported from Chivalry Engine V3
+function unformatConfig($json)
+{
+    return json_decode($json, true);
+}
+
+/**
+ * @desc Internal function to properly get module config info for use in PHP.
+ * @param string $moduleName Name of the module.
+ * @return string $moduleConfig;
+ */
+//Back ported from Chivalry Engine V3
+function getConfigForPHP(string $moduleName)
+{
+    return unformatConfig(readConfigFromDB($moduleName));
+}
