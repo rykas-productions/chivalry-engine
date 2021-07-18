d5bfb49c7b84f4b38da7ea2103cbaf2cad42167e
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-07-18 14:59:46 -0400

    Constants for the slot names.

---
+++ upload/const/const_slots.php
@@ -0,0 +1,19 @@
+<?php
+/*	File:		const_slots.php
+	Created: 	Jul 18, 2021; 1:40:11 PM
+	Info: 		Defines constant names for the equipment slots.
+	Author:		TheMasterGeneral
+	Website: 	https://chivalryisdeadgame.com/
+*/
+define('slot_prim_wep','equip_primary_weapon');
+define('slot_second_wep','equip_secondary_weapon');
+define('slot_armor','equip_armor');
+define('slot_badge','equip_badge');
+define('slot_potion','equip_badge');
+
+define('slot_prim_ring','equip_ring_primary');
+define('slot_second_ring','equip_ring_secondary');
+define('slot_necklace','equip_necklace');
+define('slot_pendant','equip_pendant');
+
+define('slot_wed_ring','equip_wedding_ring');
