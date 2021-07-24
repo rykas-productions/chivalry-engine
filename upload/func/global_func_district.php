8ab2ae9ed31af9cc9e530ab77ae7a05949002253
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-07-23 22:32:06 -0400

    PHP Dox

--- upload/func/global_func_district.php
+++ upload/func/global_func_district.php
@@ -34,8 +34,8 @@ $districtConfig['upkeepPerTile'] = 10000;
 //end module config
 
 /**
- * @internal
  * Called daily to charge guilds their daily fees.
+ * @internal
  */
 function doDailyDistrictTick()
 {
@@ -66,6 +66,11 @@ function doDailyDistrictTick()
 	}
 	districtRewards();
 }
+/**
+ * Returns the active members of the guild.
+ * @param int $guild_id Guild's ID.
+ * @return int Players in guild active within last 24 hours
+ */
 function countActiveGuildMembers24Hr($guild_id)
 {
 	global $db;
@@ -73,35 +78,68 @@ function countActiveGuildMembers24Hr($guild_id)
 	$q = $db->query("/*qc=on*/SELECT `username` FROM `users` WHERE `guild` = {$guild_id} AND `laston` > {$last_on}");
 	return $db->num_rows($q);
 }
+
+/**
+ * Returns the number of warriors deployed on the districts, owned by a specific guild.
+ * @param int $guild_id Guild's ID.
+ * @return int Warriors deployed
+ */
 function countDeployedWarriors($guild_id)
 {
 	global $db;
 	$q=$db->query("SELECT SUM(`district_melee`) FROM `guild_districts` WHERE `district_owner` = {$guild_id}");
 	return $db->fetch_single($q);
 }
+
+/**
+ * Returns the number of archers deployed on the districts, owned by a specific guild.
+ * @param int $guild_id Guild's ID.
+ * @return int Archers deployed
+ */
 function countDeployedArchers($guild_id)
 {
 	global $db;
 	$q=$db->query("SELECT SUM(`district_range`) FROM `guild_districts` WHERE `district_owner` = {$guild_id}");
 	return $db->fetch_single($q);
 }
+
+/**
+ * Returns the number of generals deployed on the districts, owned by a specific guild.
+ * @param int $guild_id Guild's ID.
+ * @return int Generals deployed
+ */
 function countDeployedGenerals($guild_id)
 {
 	global $db;
 	$q=$db->query("SELECT SUM(`district_general`) FROM `guild_districts` WHERE `district_owner` = {$guild_id}");
 	return $db->fetch_single($q);
 }
+
+/**
+ * Returns the number of districts controlled by a guild.
+ * @param int $guild_id Guild's ID.
+ * @return int Districts owned,
+ */
 function countOwnedDistricts($guild_id)
 {
     global $db;
     return $db->num_rows($db->query("SELECT `district_id` FROM `guild_districts` WHERE `district_owner` = {$guild_id}"));
 }
+
+/**
+ * Helper function to call to reward daily district awards.
+ * @internal
+ */
 function districtRewards()
 {
 	districtRewardMostControlledTiles();
 	districtRewardMostDeployedUnits();
 }
 
+/**
+ * Reward the guild with most units deployed on the districts board with 5 CID Admin Gym scrolls.
+ * @internal
+ */
 function districtRewardMostDeployedUnits()
 {
 	global $db, $api;
@@ -127,6 +165,10 @@ function districtRewardMostDeployedUnits()
 	$api->GuildAddNotification($winnerguild, "Your guild has the most deployed units on the guild districts and has received 5 {$api->SystemItemIDtoName(205)} to your armory.");
 }
 
+/**
+ * Reward the guild with most controlled tiles on the districts board with 5 CID Admin scrolls.
+ * @internal
+ */
 function districtRewardMostControlledTiles()
 {
 	global $db, $api;
@@ -151,6 +193,14 @@ function districtRewardMostControlledTiles()
 	$api->GuildAddItem($winnerguild,205,5);
 	$api->GuildAddNotification($winnerguild, "Your guild has the most controlled tiles on the guild districts and has received 5 {$api->SystemItemIDtoName(205)} to your armory.");
 }
+
+/**
+ * Tests to see if the player can access the input tile ID. This usually means if the player's guild has 
+ * a tile nearby, or if the tile itself is on the outer border.
+ * @internal
+ * @param int $district_id District ID to test
+ * @return bool Can be accessed
+ */
 function isDistrictAccessible($district_id)
 {
     global $db, $ir, $districtConfig;
@@ -194,18 +244,31 @@ function isGuildDistrict($district_id)
     $q=$db->query("SELECT `district_owner` FROM `guild_districts` WHERE `district_id` = {$district_id}");
     if ($ir['guild'] == 0)
         $guild=-1;
-        else
-            $guild=$ir['guild'];
-            $r=$db->fetch_single($q);
-            if ($r == $guild)
-                return true;
-                else
-                    return false;
+    else
+        $guild=$ir['guild'];
+    $r=$db->fetch_single($q);
+    if ($r == $guild)
+        return true;
+    else
+        return false;
 }
 
+/**
+ * Function for handling attacks in the districts.
+ * @internal
+ * @param int $attackWarrior Warriors on attack.
+ * @param int $attackArcher Archers on attack.
+ * @param int $attackCaptain Captains on attack.
+ * @param int $defenseWarrior Warriors on defense.
+ * @param int $defenseArcher Archers on defense.
+ * @param int $defenseGeneral Generals on defense.
+ * @param float $attackBuff Buff levels for the attackers, defaults to 1.0
+ * @param float $defenseBuff Buff levels for the defenders, defaults to 1.0
+ * @return string json containing attack result.
+ */
 function doAttack($attackWarrior, $attackArcher, $attackCaptain, $defenseWarrior, $defenseArcher, $defenseGeneral = 0, $attackBuff = 1.0, $defenseBuff = 1.0)
 {
-    global $api, $userid, $districtConfig;
+    global $districtConfig;
     
     $attackTotal = $attackWarrior + $attackArcher;
     $defenseTotal = $defenseWarrior + $defenseArcher;
@@ -219,6 +282,8 @@ function doAttack($attackWarrior, $attackArcher, $attackCaptain, $defenseWarrior
     $result['defense_warrior_lost'] = 0;
     $result['defense_archer_lost'] = 0;
     $result['defense_general_lost'] = 0;
+    
+    //We will continue to loop while a winner is still undecided.
     while (empty($result['winner']))
     {
         $defenderRating=0;
@@ -226,7 +291,11 @@ function doAttack($attackWarrior, $attackArcher, $attackCaptain, $defenseWarrior
         $dfndr=0;
         $fght=0;
         
-        //Pick defender
+        /*
+         * Logic to handle which units for the defense to use in this skirmish. 
+         * Has fail overs for just in case they're missing a type 
+         * of unit.
+         */
         if (($defenseWarrior > 0) && ($defenseArcher > 0))
         {
             $dfndr=mt_rand(1,2);
@@ -246,7 +315,11 @@ function doAttack($attackWarrior, $attackArcher, $attackCaptain, $defenseWarrior
         
         if ($dfndr > 0)
         {
-            //Pick attacker
+            /*
+             * Logic to handle which units for the attack to use in this skirmish.
+             * Has fail overs for just in case they're missing a type
+             * of unit.
+             */
             if (($attackWarrior > 0) && ($attackArcher > 0))
             {
                 $fght=mt_rand(1,2);
@@ -380,6 +453,12 @@ function doAttack($attackWarrior, $attackArcher, $attackCaptain, $defenseWarrior
     return json_encode($result);
 }
 
+/**
+ * Function testing if a tile can accessed from a specific tile.
+ * @param int $from_tile Tile ID to test from
+ * @param int $to_tile Tile ID to test if can be accessed.
+ * @return boolean Is Accessible from $from_tile
+ */
 function isAccessibleFromTile($from_tile, $to_tile)
 {
     global $db;
@@ -399,11 +478,24 @@ function isAccessibleFromTile($from_tile, $to_tile)
         else
             return false;
 }
+/**
+ * Helper function to quickly update a tile's ownership. Note that this destroy's the fortification level of the tile.
+ * @param int $district_id ID of the Tile you wish to update.
+ * @param int $new_owner Guild ID of the Tile's new owner.
+ */
 function setTileOwnership($district_id, $new_owner)
 {
     global $db;
     $db->query("UPDATE `guild_districts` SET `district_owner` = {$new_owner}, `district_fortify` = 0 WHERE `district_id` = {$district_id}");
 }
+
+/**
+ * Helper function to quickly update a tile's troops.
+ * @param int $district_id Tile ID to edit troops upon.
+ * @param int $warriorChange Warriors to be added or removed.
+ * @param int $archerChange Archers to be added or removed.
+ * @param int $generalChange Generals to be added or removed.
+ */
 function updateTileTroops($district_id, $warriorChange, $archerChange, $generalChange)
 {
     global $db;
@@ -413,6 +505,16 @@ function updateTileTroops($district_id, $warriorChange, $archerChange, $generalC
 				`district_general` = `district_general` + ({$generalChange})
 				WHERE `district_id` = {$district_id}");
 }
+
+
+/**
+ * Helper function to update a guild's district's barracks.
+ * @param int $guild_id Guild ID of barracks to update.
+ * @param int $warriorChange Warriors to add or remove from barracks.
+ * @param int $archerChange Archers to add or remove from barracks.
+ * @param int $generalChange Generals to add or remove from barracks.
+ * @param int $captainChange Captains to add or remove from barracks.
+ */
 function updateBarracksTroops($guild_id, $warriorChange, $archerChange, $generalChange, $captainChange)
 {
     global $db;
@@ -423,6 +525,14 @@ function updateBarracksTroops($guild_id, $warriorChange, $archerChange, $general
                 `barracks_captains` = `barracks_captains` + ({$captainChange})
 				WHERE `guild_id` = {$guild_id}");
 }
+
+/**
+ * Blocks access to certain areas if the input Guild ID is zero.
+ * Typically used to block access to things that require you to be in
+ * a guild.
+ * @param int $guild Guild ID
+ * @return boolean
+ */
 function blockAccess($guild)
 {
     if ($guild == 0)
@@ -430,6 +540,12 @@ function blockAccess($guild)
         else
             false;
 }
+
+/**
+ * Parses the input distrct ID into a human readable string in a X,Y format.
+ * @param int $district_id District ID.
+ * @return string X:x;Y:y
+ */
 function resolveCoordinates($district_id)
 {
     global $db;
@@ -437,6 +553,12 @@ function resolveCoordinates($district_id)
     $r=$db->fetch_row($q);
     return "X: {$r['district_x']}; Y: {$r['district_y']}";
 }
+/**
+ * Tests if the specified user is owner or co-owner of the specified guild
+ * @param int $guild Guild ID
+ * @param int $user User ID to test
+ * @return boolean True if part of leadership, false if not.
+ */
 function isGuildLeaders($guild, $user)
 {
     global $db;
@@ -444,11 +566,16 @@ function isGuildLeaders($guild, $user)
     $r=$db->fetch_row($q);
     if ($r['guild_owner'] == $user)
         return true;
-        elseif ($r['guild_coowner'] == $user)
+    elseif ($r['guild_coowner'] == $user)
         return true;
-        else
-            return false;
+    else
+        return false;
 }
+/**
+ * Returns a friendly string specifying the fortification level of the specified district.
+ * @param int $district_id District ID to return fortification level.
+ * @return string Roman Numerals (Up to 10)
+ */
 function returnForticationLevel($district_id)
 {
     global $db;
@@ -456,53 +583,83 @@ function returnForticationLevel($district_id)
     
     if ($q == 1)
         return "I";
-        elseif ($q == 2)
+    elseif ($q == 2)
         return "II";
-        elseif ($q == 3)
+    elseif ($q == 3)
         return "III";
-        elseif ($q == 4)
+    elseif ($q == 4)
         return "IV";
-        elseif ($q == 5)
+    elseif ($q == 5)
         return "V";
-        elseif ($q == 6)
+    elseif ($q == 6)
         return "VI";
-        elseif ($q == 7)
+    elseif ($q == 7)
         return "VII";
-        elseif ($q == 8)
+    elseif ($q == 8)
         return "VIII";
-        elseif ($q == 9)
+    elseif ($q == 9)
         return "IX";
-        elseif ($q == 10)
+    elseif ($q == 10)
         return "X";
-        else
-            return "∅";
+    else
+        return "∅";
 }
 
+/**
+ * Function to count all tiles considered a town, owned by the current user's guild. At 
+ * this time, only markets are consider town tiles.
+ * @internal
+ * @return int Town tiles owned.
+ * @todo Expand this to be not reliant on the current user
+ */
 function countTowns()
 {
     return countMarkets();
 }
 
+/**
+ * Function to count all tiles considered a market, owned by the current user's guild.
+ * @return int Markets owned
+ * @internal
+ * @todo Expand this to be not reliant on the current user
+ */
 function countMarkets()
 {
     global $db, $ir;
     return $db->fetch_single($db->query("SELECT COUNT(`district_id`) FROM `guild_districts` WHERE `district_type` = 'market' AND `district_owner` = {$ir['guild']}"));
 }
 
+/**
+ * Counts all tiles considered an outpost, owned by the current user's guild.
+ * @internal
+ * @return int Outposts owned.
+ * @todo Expand this to be not reliant on the current user
+ */
 function countOutposts()
 {
     global $db, $ir;
     return $db->fetch_single($db->query("SELECT COUNT(`district_id`) FROM `guild_districts` WHERE `district_type` = 'outpost' AND `district_owner` = {$ir['guild']}"));
 }
 
+/**
+ * Counts all active generals, archers and warriors on district tiles, owned by the current user's guild.
+ * @return int Total troops
+ * @todo Expand this to be not reliant on the current user
+ */
 function countActiveTroops()
 {
     global $db, $ir;
     $warriors = $db->fetch_single($db->query("SELECT SUM(`district_melee`) FROM `guild_districts` WHERE `district_owner` = {$ir['guild']}"));
     $archers = $db->fetch_single($db->query("SELECT SUM(`district_range`) FROM `guild_districts` WHERE `district_owner` = {$ir['guild']}"));
-    return $archers + $warriors;
+    $generals = $db->fetch_single($db->query("SELECT SUM(`district_general`) FROM `guild_districts` WHERE `district_owner` = {$ir['guild']}"));
+    return $archers + $warriors + $generals;
 }
 
+/**
+ * Counts the current guild's total generals.
+ * @return int Total generals
+ * @todo Rewrite to not rely on the current user's guild.
+ */
 function countGenerals()
 {
     global $db, $ir;
@@ -511,6 +668,12 @@ function countGenerals()
     return $deployed + $barracks;
 }
 
+/**
+ * Counts the current guild's total captains.
+ * Captains can only be stored in the barracks.
+ * @return int Captains owned.
+ * @todo Rewrite to not rely on current user's guild.
+ */
 function countCaptains()
 {
     global $db, $ir;
@@ -518,6 +681,24 @@ function countCaptains()
     return $barracks;
 }
 
+/**
+ * Function to log guild district attacks into the database
+ * @param int $attacker Guild ID of the attacking guild.
+ * @param int $defender Guild ID of defending guild.
+ * @param int $att_war Attacking guild's warriors.
+ * @param int $att_range Attacking guild's archers
+ * @param int $att_war_lost Attacking guild's fallen warriors.
+ * @param int $att_range_lost Attacking guild's fallen archers.
+ * @param int $def_war Defender guild's warriors.
+ * @param int $def_range Defender guild's archers
+ * @param int $def_war_lost Defender guild's warriors fallen
+ * @param int $def_range_lost Defender guild's fallen archers
+ * @param int $def_general Defender guild's generals
+ * @param int $fortify Defending's tile's fortification level
+ * @param int $winner Guild ID of winner of battle.
+ * @param int $att_capt Captains deployed by ataccker guild.
+ * @return int Battle Log ID.
+ */
 function logBattle($attacker, $defender, $att_war, $att_range, $att_war_lost, $att_range_lost, $def_war, $def_range, $def_war_lost, $def_range_lost, $def_general, $fortify, $winner, $att_capt)
 {
     global $db;
@@ -534,9 +715,15 @@ function logBattle($attacker, $defender, $att_war, $att_range, $att_war_lost, $a
     return $db->insert_id();
 }
 
+/**
+ * Function to display tiles. We may pass extra data to display extra info.
+ * @param int $districtID District
+ * @param string $extra Data to display more links, can be empty.
+ * @return object Creates the tile.
+ */
 function parseTile(int $districtID, $extra = '')
 {
-    global $db, $ir, $api;
+    global $db, $api;
     $id=$db->query("SELECT * FROM `guild_districts` WHERE `district_id` = {$districtID}");
     if ($db->num_rows($id) > 0)
     {
@@ -599,6 +786,11 @@ function parseTile(int $districtID, $extra = '')
     }
 }
 
+/**
+ * Parses the tile to the North West of the current selected tile.
+ * @param int $districtID Tile ID being looked at currently.
+ * @param string $extra
+ */
 function parseNWtile(int $districtID, $extra = '')
 {
     global $db;
@@ -615,6 +807,11 @@ function parseNWtile(int $districtID, $extra = '')
     }
 }
 
+/**
+ * Parses the tile to the North of the current selected tile.
+ * @param int $districtID Tile ID being looked at currently.
+ * @param string $extra
+ */
 function parseNtile(int $districtID, $extra = '')
 {
     global $db;
@@ -631,6 +828,11 @@ function parseNtile(int $districtID, $extra = '')
     }
 }
 
+/**
+ * Parses the tile to the North East of the current selected tile.
+ * @param int $districtID Tile ID being looked at currently.
+ * @param string $extra
+ */
 function parseNEtile(int $districtID, $extra = '')
 {
     global $db;
@@ -646,6 +848,12 @@ function parseNEtile(int $districtID, $extra = '')
         }
     }
 }
+
+/**
+ * Parses the tile to the West of the current selected tile.
+ * @param int $districtID Tile ID being looked at currently.
+ * @param string $extra
+ */
 function parseWtile(int $districtID, $extra = '')
 {
     global $db;
@@ -662,6 +870,11 @@ function parseWtile(int $districtID, $extra = '')
     }
 }
 
+/**
+ * Parses the tile to the East of the current selected tile.
+ * @param int $districtID Tile ID being looked at currently.
+ * @param string $extra
+ */
 function parseEtile(int $districtID, $extra = '')
 {
     global $db;
@@ -678,6 +891,11 @@ function parseEtile(int $districtID, $extra = '')
     }
 }
 
+/**
+ * Parses the tile to the South West of the current selected tile.
+ * @param int $districtID Tile ID being looked at currently.
+ * @param string $extra
+ */
 function parseSWtile(int $districtID, $extra = '')
 {
     global $db;
@@ -694,6 +912,11 @@ function parseSWtile(int $districtID, $extra = '')
     }
 }
 
+/**
+ * Parses the tile to the South of the current selected tile.
+ * @param int $districtID Tile ID being looked at currently.
+ * @param string $extra
+ */
 function parseStile(int $districtID, $extra = '')
 {
     global $db;
@@ -710,6 +933,11 @@ function parseStile(int $districtID, $extra = '')
     }
 }
 
+/**
+ * Parses the tile to the South East of the current selected tile.
+ * @param int $districtID Tile ID being looked at currently.
+ * @param string $extra
+ */
 function parseSEtile(int $districtID, $extra = '')
 {
     global $db;
@@ -726,6 +954,9 @@ function parseSEtile(int $districtID, $extra = '')
     }
 }
 
+/**
+ * Helper function to help the district tiles look significantly better.
+ */
 function returnTileClass($district)
 {
     global $db, $ir;
