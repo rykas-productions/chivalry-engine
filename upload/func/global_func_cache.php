c25de2f4b6f501a6a8e6cbae37ff368734a04ef1
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-09-05 17:02:38 -0400

    fix lmao

--- upload/func/global_func_cache.php
+++ upload/func/global_func_cache.php
@@ -203,3 +203,26 @@ function recache_forum($forum)
     }
     echo " ... Recaching completed.<br />";
 }
+
+/**
+ * Internal version check function for Chivalry Engine.
+ * @param string $url URL for version checker. (Default = https://raw.githubusercontent.com/MasterGeneral156/Version/master/chivalry-engine.json)
+ * @return string Update Status
+ */
+function version_json($url = 'https://raw.githubusercontent.com/MasterGeneral156/Version/master/chivalry-engine.json')
+{
+    global $set;
+    $engine_version = $set['Version_Number'];
+    $json = json_decode(get_cached_file($url, "./cache/update_check.json"), true);
+    if (is_null($json))
+        return "Update checker failed.";
+        if (version_compare($engine_version, $json['latest']) == 0 || version_compare($engine_version, $json['latest']) == 1)
+            return "Chivalry Engine is up to date.";
+            else
+                return "Chivalry Engine update available. Download it <a href='{$json['download-latest']}'>here</a>.";
+}
+
+function fetchCIDDB($hours)
+{
+    return get_cached_file("https://chivalryisdeadgame.com/cache/latest.sql", "./cache/latest.sql", 24);
+}
