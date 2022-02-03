8ef2470a8fb0f4201da08bba70d28b12d0cc1ec1
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-02-03 10:02:12 -0500

    Facelift

--- upload/academy.php
+++ upload/academy.php
@@ -42,7 +42,7 @@ if ($ir['course'] > 0)  //User is enrolled in a course, so lets tell them and st
 	$percentcomplete=round(($starttime/$daystoseconds)*100);
 	if (isset($_GET['dropout']))
 	{
-		if ($percentcomplete <= 5)
+		if ($percentcomplete <= 33)
 		{
 			addToEconomyLog('Academy', 'copper', $coud['ac_cost']);
 			$db->query("UPDATE `users` 
@@ -50,10 +50,10 @@ if ($ir['course'] > 0)  //User is enrolled in a course, so lets tell them and st
 						`course` = 0, 
 						`course_complete` = 0 
 						WHERE `userid` = {$userid}");
-			alert("success","Success!","You have successfully dropped out of your course. You have been refunded {$coud['ac_cost']} Copper Coins.",true,'academy.php');
+			alert("success","Success!","You have successfully dropped out of your course. You have been refunded " . shortNumberParse($coud['ac_cost']) . " Copper Coins.",true,'academy.php');
 			die($h->endpage());
 		}
-		elseif (($percentcomplete > 5) && ($percentcomplete <= 60))
+		elseif (($percentcomplete > 33) && ($percentcomplete <= 66))
 		{
 			$db->query("UPDATE `users` 
 						SET `course` = 0, 
@@ -67,11 +67,29 @@ if ($ir['course'] > 0)  //User is enrolled in a course, so lets tell them and st
 			alert("danger","Uh Oh!","You are too far into your course to dropout now.",true,'academy.php');
 		}
 	}
-    echo "You are currently enrolled in the {$coud['ac_name']} course. You will be finished in " . TimeUntil_Parse($ir['course_complete']) . ".<br />
-	You may dropout of this course. Please note that you can only dropout if you've completed less than 60% of the course. If you dropout before 5% 
-	completion, you will be refunded all your cash. Otherwise, you will lose all the cash you used to enroll. <br /><b>You have currently completed {$percentcomplete}% of 
-	this course.</b><br />
-	Do you wish to <a href='?dropout=yes'>dropout</a>?";
+	echo "<div class='row'>
+        <div class='col-12'>
+            <div class='card'>
+                <div class='card-header'>
+                    You are currently enrolled in the {$coud['ac_name']} course.
+                </div>
+                <div class='card-body'>
+                    The course will be completed in " . TimeUntil_Parse($ir['course_complete']) . ".<br />
+                    You may dropout of this course. Please note that you can only dropout if you've completed 
+                    less than 66% of the course. If you dropout before 33% completion, you will be refunded all 
+                    your cash. Otherwise, you will lose all the Copper Coins you used to enroll.<br />
+                    <div class='progress' style='height: 1rem;'>
+				        <div class='progress-bar bg-success progress-bar-striped progress-bar-animated' role='progressbar' aria-valuenow='{$percentcomplete}%' style='width:{$percentcomplete}%' aria-valuemin='0' aria-valuemax='100'>
+        					<span>
+        						{$percentcomplete}% Completed
+        					</span>
+				        </div>
+			         </div>
+                    Do you wish to <a href='?dropout=yes'>dropout</a>?
+                </div>
+            </div>
+        </div>
+    </div>";
     die($h->endpage());
 }
 if (!isset($_GET['action'])) {
@@ -105,10 +123,10 @@ function menu()
         //If user has already completed the course.
         if ($db->fetch_single($cdo) > 0)
             $do = "<a href='#' class='disabled btn-success btn btn-block'>Graduated</a>";
+        elseif ($ir['level'] < $academy['ac_level'])
+            $do = "<a href='#' class='disabled btn-danger btn btn-block'>Level too low</a>";
 		elseif ($ir['primary_currency'] < $academy['ac_cost'])
 			$do = "<a href='#' class='disabled btn-danger btn btn-block'>Not enough Copper</a>";
-		elseif ($ir['level'] < $academy['ac_level'])
-			$do = "<a href='#' class='disabled btn-danger btn btn-block'>Level too low</a>";
 		else
             $do = "<a href='?action=start&id={$academy['ac_id']}' class='btn btn-primary btn-block'>Start Course</a>";
 		echo "
