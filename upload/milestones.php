745f2dcd49a533964beeeae5c36e79102f09f7f9
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-11-19 10:45:23 -0500

    Update milestones.php

--- upload/milestones.php
+++ upload/milestones.php
@@ -4,156 +4,148 @@ echo "<h3>CID Milestones</h3><hr />
 The following is a list of milestones that players have accomplished inside of Chivalry is Dead. This
  won't be constantly updated sadly. It'll only be updated when someone accomplishes something note
  worthy.
- </hr >
- <table class='table table-bordered'>
-    <tr>
-        <th>
-            Milestone
-        </th>
-        <th>
-            Accomplisher
-        </th>
-    </tr>
-    <tr>
-        <td>
-            2017 Thanksgiving Trivia Winner
-        </td>
-        <td>
-            <a href='profile.php?user=143'>" . parseUsername(143) . " [143]</a>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            2018 Referral Contest Winner
-        </td>
-        <td>
-            <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            2018 Thanksgiving Trivia Winner
-        </td>
-        <td>
-            <a href='profile.php?user=220'>" . parseUsername(220) . " [220]</a>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            2019 Big Bang Event Victims
-        </td>
-        <td>
-            <a href='profile.php?user=184'>" . parseUsername(184) . " [184]</a><br />
-			<a href='profile.php?user=102'>" . parseUsername(102) . " [102]</a><br />
-			<a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            2020 Big Bang Event Victims
-        </td>
-        <td>
-            <a href='profile.php?user=406'>" . parseUsername(406) . " [406]</a><br />
-			<a href='profile.php?user=433'>" . parseUsername(433) . " [433]</a><br />
-			<a href='profile.php?user=181'>" . parseUsername(181) . " [181]</a>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            First Mastery Rank I
-        </td>
-        <td>
-            <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            First Mastery Rank II
-        </td>
-        <td>
-            <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            First Mastery Rank III
-        </td>
-        <td>
-            <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            First Mastery Rank IV
-        </td>
-        <td>
-			<a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            First Mastery Rank V
-        </td>
-        <td>
-			<a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            First to Level 1,000
-        </td>
-        <td>
-            <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            First to Level 1,500
-        </td>
-        <td>
-            <i>null</i>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            First to Level 2,000
-        </td>
-        <td>
-            <i>null</i>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            First <i>Enlightened Warrior</i> graduate
-        </td>
-        <td>
-            <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            1st Guild to level 10
-        </td>
-        <td>
-			<a href='guilds.php?action=view&id=8'>Purgatory [8]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            1st guild to level 20
-        </td>
-        <td>
-            <a href='guilds.php?action=view&id=8'>Purgatory [8]</a>
-        </td>
-    </tr>
-	<tr>
-        <td>
-            1st guild to level 50
-        </td>
-        <td>
-            <i>null</i>
-        </td>
-    </tr>
- </table>";
+<div class='row'>
+    <div class='col-12 col-md-6 col-xxl-4 col-xxxl-3'>
+        <div class='card'>
+            <div class='card-header'>
+                2022 Halloween Pumpkin Chuck
+            </div>
+            <div class='card-body'>
+                <a href='profile.php?user=143'>" . parseUsername(143) . " [143]</a>
+            </div>
+        </div>
+    </div>
+    <div class='col-12 col-md-6 col-xxl-4 col-xxxl-3'>
+        <div class='card'>
+            <div class='card-header'>
+                2017 Thanksgiving Trivia Winner
+            </div>
+            <div class='card-body'>
+                <a href='profile.php?user=143'>" . parseUsername(143) . " [143]</a>
+            </div>
+        </div>
+    </div>
+    <div class='col-12 col-md-6 col-xxl-4 col-xxxl-3'>
+        <div class='card'>
+            <div class='card-header'>
+                2018 Thanksgiving Trivia Winner
+            </div>
+            <div class='card-body'>
+                <a href='profile.php?user=220'>" . parseUsername(220) . " [220]</a>
+            </div>
+        </div>
+    </div>
+    <div class='col-12 col-md-6 col-xxl-4 col-xxxl-3'>
+        <div class='card'>
+            <div class='card-header'>
+                2018 Referral Contest Winner
+            </div>
+            <div class='card-body'>
+                <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
+            </div>
+        </div>
+    </div>
+    <div class='col-12 col-md-6 col-xxl-4 col-xxxl-3'>
+        <div class='card'>
+            <div class='card-header'>
+                2019 Big Bang Event Victims
+            </div>
+            <div class='card-body'>
+                <div class='row'>
+                    <div class='col-12 col-sm-6'>
+                        <a href='profile.php?user=184'>" . parseUsername(184) . " [184]</a>
+                    </div>
+                    <div class='col-12 col-sm-6'>
+                        <a href='profile.php?user=102'>" . parseUsername(102) . " [102]</a></a>
+                    </div>
+                    <div class='col-12 col-sm-6'>
+                        <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class='col-12 col-md-6 col-xxl-4 col-xxxl-3'>
+        <div class='card'>
+            <div class='card-header'>
+                2020 Big Bang Event Victims
+            </div>
+            <div class='card-body'>
+                <div class='row'>
+                    <div class='col-12 col-sm-6'>
+                        <a href='profile.php?user=406'>" . parseUsername(406) . " [406]</a>
+                    </div>
+                    <div class='col-12 col-sm-6'>
+                        <a href='profile.php?user=433'>" . parseUsername(433) . " [433]</a></a>
+                    </div>
+                    <div class='col-12 col-sm-6 col-lg'>
+                        <a href='profile.php?user=181'>" . parseUsername(181) . " [181]</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class='col-12 col-md-6 col-lg col-xl-6 col-xxl'>
+        <div class='card'>
+            <div class='card-header'>
+                Mastery Rank
+            </div>
+            <div class='card-body'>
+                <div class='row'>
+                    <div class='col-12 col-sm-6 col-lg-4 col-xl-6 col-xxl'>
+                        <div class='row'>
+                            <div class='col-12'>
+                                <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
+                            </div>
+                            <div class='col-12 text-muted small'>
+                                1st Mastery Rank I
+                            </div>
+                        </div>
+                    </div>
+                    <div class='col-12 col-sm-6 col-lg-4 col-xl-6 col-xxl'>
+                        <div class='row'>
+                            <div class='col-12'>
+                                <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
+                            </div>
+                            <div class='col-12 text-muted small'>
+                                1st Mastery Rank II
+                            </div>
+                        </div>
+                    </div>
+                    <div class='col-12 col-sm-6 col-lg-4 col-xl-6 col-xxl'>
+                        <div class='row'>
+                            <div class='col-12'>
+                                <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
+                            </div>
+                            <div class='col-12 text-muted small'>
+                                1st Mastery Rank III
+                            </div>
+                        </div>
+                    </div>
+                    <div class='col-12 col-sm-6 col-lg col-xl-6 col-xxl'>
+                        <div class='row'>
+                            <div class='col-12'>
+                                <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
+                            </div>
+                            <div class='col-12 text-muted small'>
+                                1st Mastery Rank IV
+                            </div>
+                        </div>
+                    </div>
+                    <div class='col-12 col-sm-6 col-lg col-xl-6 col-xxl'>
+                        <div class='row'>
+                            <div class='col-12'>
+                                <a href='profile.php?user=161'>" . parseUsername(161) . " [161]</a>
+                            </div>
+                            <div class='col-12 text-muted small'>
+                                1st Mastery Rank V
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>";
  
  $h->endpage();
