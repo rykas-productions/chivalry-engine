95cb1051d68474f05c9adc8890e21b3bfd0b793b
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-11-19 10:44:53 -0500

    Update hour.php

--- upload/crons/hour.php
+++ upload/crons/hour.php
@@ -33,4 +33,8 @@ $db->query("UPDATE `user_settings` SET `searchtown` = 100 WHERE `searchtown` > 1
 runMarketTick(3);   //med risk market
 if ((date('G') == 6) || (date('G') == 12) || (date('G') == 18))
     runMarketTick(2);   //lower risk
+
+    //halloween 2022
+    $db->query("UPDATE `2018_halloween_chuck` SET `count` = `count` + 2 WHERE `count` < 20");
+    $db->query("UPDATE `2018_halloween_chuck` SET `count` = 20 WHERE `count` > 20");
 ?>
