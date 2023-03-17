e2450308e69881a03c0c30d745c5c20d472288c1
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-03-17 15:43:40 -0400

    Update missions.php

--- upload/missions.php
+++ upload/missions.php
@@ -11,7 +11,7 @@ if ($db->num_rows($am) == 0)
 {
 	if (isset($_GET['accept']))
 	{
-		$days=Random(1,3);
+		$days=Random(1, 7);   //upped from 3
 		$kills=(Random(5,15)+Random($ir['level']/4,$ir['level']/2))*$days;
 		$reward = 0;
 		$loops = 0;
@@ -21,11 +21,32 @@ if ($db->num_rows($am) == 0)
 		    $reward = $reward + round($random + ($random * levelMultiplier($ir['level'], $ir['reset'])));
 		    $loops++;
 		}
-		$endtime=time()+($days*86400);
+		$endtime=time()+($days*86400);    //create end timestamp in seconds
 		$db->query("INSERT INTO `missions` 
 		(`mission_userid`, `mission_kills`, 
 		`mission_end`, `mission_kill_count`, `mission_reward`) 
 		VALUES ('{$userid}', '{$kills}', '{$endtime}', '0', '{$reward}')");
+		//tablet coding is pita but doable...
+		echo "<div class='card'>
+                <div class='card-header'>
+                    Current Mission
+                </div>
+                <div class='card-body'>
+                    <div class='row'>
+                        <div class='col'>
+                            <div class='row'>
+                                 <div class='col-12'>
+                                    <small>Kills Required</small>
+                                </div>
+                                <div class='col-12'>
+                                    " . shortNumberParse($kills) . "
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+		
 		echo "Your current mission:<br />
 		Kills Required: " . number_format($kills) . "<br />
 		Kill Count: 0<br />
