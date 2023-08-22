db0d585559db64972c06b3c7bb43c35433eade40
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-08-21 20:23:21 -0400

    Update gamerules.php

--- upload/gamerules.php
+++ upload/gamerules.php
@@ -7,17 +7,32 @@
 	Website: 	https://github.com/MasterGeneral156/chivalry-engine
 */
 require('globals.php');
-echo "<h3>{$set['WebsiteName']} Game Rules</h3>
-<hr />
-You are expected to follow this rules. You are also expected to check back on these fairly frequently as these rules
-may change without notice. Staff will not accept ignorance as an excuse if you break one of these rules.<hr />";
+alert('info', "", "You are expected to follow this rules. You are also expected to check back on these fairly frequently as these rules
+may change without notice. Staff will not accept ignorance as an excuse if you break one of these rules.",  false);
+echo "<div class='card'>
+        <div class='card-header'>
+            {$set['WebsiteName']} Game Rules
+        </div>
+        <div class='card-body'>";
 $q = $db->query("/*qc=on*/SELECT * FROM `gamerules` ORDER BY `rule_id` ASC");
 $rulenumber = 1;
-echo "<dl>";
 //List game rules.
-while ($r = $db->fetch_row($q)) {
-    echo "<dd><b>Rule #{$rulenumber}<br /></b>{$r['rule_text']}</dd><hr />";
+while ($r = $db->fetch_row($q)) 
+{
+    echo "  <div class='row'>
+                <div class='col-auto'>
+                    <div class='row'>
+                        <div class='col-12'>
+                            <small><b>Rule #{$rulenumber}</b></small>
+                        </div>
+                        <div class='col-12'>
+                            {$r['rule_text']}
+                        </div>
+                    </div>
+                    <br />
+                </div>
+            </div>";
 	$rulenumber = $rulenumber + 1;
 }
-echo "</dl>";
+echo "</div></div>";
 $h->endpage();
