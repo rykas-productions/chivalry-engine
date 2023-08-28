aaa7fb74509d2cda8a020f2c3ddc98addbae58ce
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-08-28 19:20:26 -0400

    Update dailyreward.php

--- upload/dailyreward.php
+++ upload/dailyreward.php
@@ -8,48 +8,56 @@
  */
 if ($ir['rewarded'] == 0)
 {
+    if ($ir['dayslogged'] == 0)
+        $daysLoggedMulti = 1;
+    else
+        $daysLoggedMulti = 1 + ($ir['dayslogged'] * 0.1);
+    $daysLoggedMulti = clamp($daysLoggedMulti, 1, 31);
     $rng=Random(1,5);
     $tokenreward=(1500*$rng)+((1500*$rng)*levelMultiplier($ir['level']));
-    $medReward=Random(5,15);
-    $scrollreward=Random(10,20);
+    $tokenreward = $tokenreward * $daysLoggedMulti;
+    $medReward=Random(5 * $daysLoggedMulti, 15 * $daysLoggedMulti);
+    $scrollreward=Random(10 * $daysLoggedMulti,20 * $daysLoggedMulti);
     $luck=Random(10,35);
     $reward=Random(1,100);
     $copper=1000000+(1000000*levelMultiplier($ir['level']));
-    $bor=Random(500,1025);
+    $copper = $copper  * $daysLoggedMulti;
+    $bor=Random(500* $daysLoggedMulti,1025 * $daysLoggedMulti);
     $guardTime=Random(15,40);
     $autoHex=Random(50,105);
-    $rrBet=Random(50000,1250000);
+    $rrBet=Random(50000 * $daysLoggedMulti,1250000 * $daysLoggedMulti);
+    $skill = round(1  * $daysLoggedMulti);
     //Give skill point.
     if ($reward == 99 || $reward == 100)
     {
-        $db->query("UPDATE `user_settings` SET `skill_points` = `skill_points` + 1 WHERE `userid` = {$userid}");
-        $api->GameAddNotification($userid,"You have gained one skill point for logging into Chivalry is Dead today. Thank you!");
-        $api->SystemLogsAdd($userid, "loginreward", "Received 1 Skill Point.");
+        $db->query("UPDATE `user_settings` SET `skill_points` = `skill_points` + {$skill} WHERE `userid` = {$userid}");
+        $api->GameAddNotification($userid,"You have gained {$skill} skill point(s) for logging into Chivalry is Dead today. Thank you!");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$skill} Skill Point.");
     }
     elseif ($reward <= 98 && $reward > 90)
     {
         $api->UserGiveItem($userid,207,$medReward);
-        $api->GameAddNotification($userid,"While logging into Chivalry is Dead today, you were gifted {$medReward} Priority Vouchers, used at the infirmary. Consider it a gift from us. ;)");
-        $api->SystemLogsAdd($userid, "loginreward", "Received {$medReward} Priority Vouchers.");
+        $api->GameAddNotification($userid,"While logging into Chivalry is Dead today, you were gifted " . shortNumberParse($medReward) . " Priority Vouchers, used at the infirmary. Consider it a gift from us. ;)");
+        $api->SystemLogsAdd($userid, "loginreward", "Received " . shortNumberParse($medReward) . " Priority Vouchers.");
     }
     elseif ($reward <= 89 && $reward > 85)
     {
-        $api->UserGiveItem($userid,284,1);
-        $api->GameAddNotification($userid,"Overnight, your guild had robbed a local shop. They've returned and your gift was an Amulet of Criminal Mischief. Thank you for playing Chivalry is Dead!");
-        $api->SystemLogsAdd($userid, "loginreward", "Received Amulet of Crminal Mischief.");
+        $api->UserGiveItem($userid,284,$skill);
+        $api->GameAddNotification($userid,"Overnight, your guild had robbed a local shop. They've returned and your gift is " . shortNumberParse($skill) . " Amulet of Criminal Mischief(s). Thank you for playing Chivalry is Dead!");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$skill}x Amulet of Crminal Mischief(s).");
     }
     elseif ($reward <= 84 && $reward > 75)
     {
         $db->query("UPDATE `user_settings` SET `rickroll` = {$userid} WHERE `userid` = {$userid}");
         $api->UserGiveItem($userid,33,$bor);
         $api->GameAddNotification($userid,"You may have just been rickrolled, but we've given you " . shortNumberParse($bor) . " Boxes of Random. Just cuz! Thanks for playing Chivalry is Dead.");
-        $api->SystemLogsAdd($userid, "loginreward", "Received " . number_format($bor) . " Boxes of Random and a Rick Roll.");
+        $api->SystemLogsAdd($userid, "loginreward", "Received " . shortNumberParse($bor) . " Boxes of Random and a Rick Roll.");
     }
     elseif ($reward <= 74 && $reward > 70)
     {
-        $api->UserGiveItem($userid,259,1);
-        $api->GameAddNotification($userid,"For logging into Chivalry is Dead today, we gave you a Marriage Rose. Hint, hint. :) We love you {$ir['username']}. We love you. < 3");
-        $api->SystemLogsAdd($userid, "loginreward", "Received Marriage Rose.");
+        $api->UserGiveItem($userid,259,$skill);
+        $api->GameAddNotification($userid,"For logging into Chivalry is Dead today, we gave you {$skill} Marriage Rose(s). Hint, hint. :) We love you {$ir['username']}. We love you. < 3");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$skill} Marriage Rose.");
     }
     elseif ($reward <= 69 && $reward > 65)
     {
@@ -67,7 +75,7 @@ if ($ir['rewarded'] == 0)
     {
         $db->query("UPDATE `user_settings` SET `autohex` = `autohex` + {$autoHex} WHERE `userid` = {$userid}");
         $api->GameAddNotification($userid,"For your dedication of logging into Chivalry is Dead today, we've given you {$autoHex} Auto Hexbags for you to use. We appreciate you playing.");
-        $api->SystemLogsAdd($userid, "loginreward", "Received {$autoHex} Automatic Hexbags.");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$autoHex} Automatic Hexbags");
     }
     elseif ($reward <= 52 && $reward > 50)
     {
@@ -78,37 +86,37 @@ if ($ir['rewarded'] == 0)
             else
                 userGiveEffect($userid, constant("invisibility"), $newTime);
                 $api->GameAddNotification($userid,"We've given you {$formatTime} minutes of invisibility for your daily log in reward.");
-                $api->SystemLogsAdd($userid, "loginreward", "Received {$formatTime} minutes invisibility.");
+                $api->SystemLogsAdd($userid, "loginreward", "Received {$formatTime} minutes invisibility");
     }
     elseif ($reward <= 49 && $reward > 47)
     {
-        $api->UserGiveItem($userid,285,1);
-        $api->GameAddNotification($userid,"You've received a Potion of Permanent Strength for logging into Chivalry is Dead today. Thanks a lot!");
-        $api->SystemLogsAdd($userid, "loginreward", "Received Potion of Permanent Strength.");
+        $api->UserGiveItem($userid,285,$skill);
+        $api->GameAddNotification($userid,"You've received {$skill} x Potion of Permanent Strength(s) for logging into Chivalry is Dead today. Thanks a lot!");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$skill} x Potion of Permanent Strength(s)");
     }
     elseif ($reward <= 46 && $reward > 44)
     {
-        $api->UserGiveItem($userid,286,1);
-        $api->GameAddNotification($userid,"You've received a Potion of Everlasting Speed for logging into Chivalry is Dead today. Thanks a lot!");
-        $api->SystemLogsAdd($userid, "loginreward", "Received Potion of Everlasting Speed.");
+        $api->UserGiveItem($userid,286,$skill);
+        $api->GameAddNotification($userid,"You've received {$skill} x Potion of Everlasting Speed(s) for logging into Chivalry is Dead today. Thanks a lot!");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$skill} x Potion of Everlasting Speed(s)");
     }
     elseif ($reward <= 43 && $reward > 41)
     {
-        $api->UserGiveItem($userid,287,1);
-        $api->GameAddNotification($userid,"You've received a Potion of Youthful Tolerance for logging into Chivalry is Dead today. Thanks a lot!");
-        $api->SystemLogsAdd($userid, "loginreward", "Received Potion of Youthful Tolerance.");
+        $api->UserGiveItem($userid,287,$skill);
+        $api->GameAddNotification($userid,"You've received {$skill} x Potion of Youthful Tolerance(s) for logging into Chivalry is Dead today. Thanks a lot!");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$skill} x Potion of Youthful Tolerance(s)");
     }
     elseif ($reward <= 40 && $reward > 38)
     {
-        $api->UserGiveItem($userid,148,1);
-        $api->GameAddNotification($userid,"I'm sorry for the crappy gift... but all we have is this old Tome of Experience we don't have a use for... so here, have it. Thanks for playing CID. :/");
-        $api->SystemLogsAdd($userid, "loginreward", "Received Tome of Experience.");
+        $api->UserGiveItem($userid,148,$skill);
+        $api->GameAddNotification($userid,"I'm sorry for the crappy gift... but all we have are these old {$skill} x Tome of Experience(s) we don't have a use for... so here, have it. Thanks for playing CID. :/");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$skill} x Tome of Experience(s)");
     }
     elseif ($reward <= 37 && $reward > 26)
     {
         $api->UserGiveCurrency($userid,'secondary',$tokenreward);
         $api->GameAddNotification($userid,"We've given you " . shortNumberParse($tokenreward) . " Chivalry Tokens for logging into Chivalry is Dead today. Thanks~");
-        $api->SystemLogsAdd($userid, "loginreward", "Received " . number_format($tokenreward) . " Chivalry Tokens.");
+        $api->SystemLogsAdd($userid, "loginreward", "Received " . number_format($tokenreward) . " Chivalry Tokens");
     }
     elseif ($reward <= 25 && $reward > 23)
     {
@@ -143,22 +151,22 @@ if ($ir['rewarded'] == 0)
         {
             $newAddToken = $ir['tokenbank']/50;
             $db->query("UPDATE `users` SET `tokenbank`=`tokenbank`+(`tokenbank`/50) WHERE `tokenbank`>0 AND `userid` = {$userid}");
-            $api->GameAddNotification($userid,"We <i>accidentally</i> ran 2% interest on your Chivalry Token account. Sorry! However, for the mess up, we'll allow you to keep the extra Chivalry Tokens in the account. Thanks for playing Chivalry is Dead!");
-            $api->SystemLogsAdd($userid, "loginreward", "Received 2% Token Bank account interest.");
+            $api->GameAddNotification($userid,"We <i>accidentally</i> ran 2% interest on your Chivalry Token account. Sorry! However, for the mess up, we'll allow you to keep the extra " . shortNumberParse($newAddToken) . " Chivalry Tokens in the account. Thanks for playing Chivalry is Dead!");
+            $api->SystemLogsAdd($userid, "loginreward", "Received 2% Token Bank account interest. (" . shortNumberParse($newAddToken) . " Tokens)");
         }
         elseif (($ir['tokenbank'] > 100000) && ($ir['tokenbank'] <= 500000))
         {
             $newAddToken = $ir['tokenbank']/100;
             $db->query("UPDATE `users` SET `tokenbank`=`tokenbank`+(`tokenbank`/100) WHERE `tokenbank`>0 AND `userid` = {$userid}");
-            $api->GameAddNotification($userid,"We <i>accidentally</i> ran 1% interest on your Chivalry Token account. Sorry! However, for the mess up, we'll allow you to keep the extra Chivalry Tokens in the account. Thanks for playing Chivalry is Dead!");
-            $api->SystemLogsAdd($userid, "loginreward", "Received 1% Token Bank account interest.");
+            $api->GameAddNotification($userid,"We <i>accidentally</i> ran 1% interest on your Chivalry Token account. Sorry! However, for the mess up, we'll allow you to keep the extra " . shortNumberParse($newAddToken) . " Chivalry Tokens in the account. Thanks for playing Chivalry is Dead!");
+            $api->SystemLogsAdd($userid, "loginreward", "Received 1% Token Bank account interest");
         }
         else
         {
             $newAddToken = 5000;
             $db->query("UPDATE `users` SET `tokenbank`=`tokenbank`+5000 WHERE `tokenbank`>0 AND `userid` = {$userid}");
             $api->GameAddNotification($userid,"We've given you 5,000 Chivalry Tokens to your Chivalry Token bank account, just for logging into the game! Thank you!");
-            $api->SystemLogsAdd($userid, "loginreward", "Received 5,000 Chivalry Tokens to their Chivalry Token account.");
+            $api->SystemLogsAdd($userid, "loginreward", "Received 5,000 Chivalry Tokens to their Chivalry Token account. (" . shortNumberParse($newAddToken) . " Tokens)");
         }
         addToEconomyLog('Daily Reward', 'token', $newAddToken);
     }
@@ -166,14 +174,14 @@ if ($ir['rewarded'] == 0)
     {
         $db->query("INSERT INTO `russian_roulette` (`challengee`, `challenger`, `reward`) VALUES ('{$userid}', '1', '{$rrBet}');");
         $api->GameAddNotification($userid,"We've issued you a Russian Roulette challenge. If you win, you get " . shortNumberParse($rrBet) . " Copper Coins! Consider this a fun, risky login reward.");
-        $api->SystemLogsAdd($userid, "loginreward", "Received Russian Roulette challenges.");
+        $api->SystemLogsAdd($userid, "loginreward", "Received Russian Roulette challenges");
         addToEconomyLog('Daily Reward', 'copper', $rrBet);
     }
     else
     {
         $api->UserGiveItem($userid,227,1);
-        $api->GameAddNotification($userid,"You've received a {$api->SystemItemIDtoName(227)} for logging into Chivalry is Dead today. Thanks a lot!");
-        $api->SystemLogsAdd($userid, "loginreward", "Received {$api->SystemItemIDtoName(227)}.");
+        $api->GameAddNotification($userid,"You've received {$skill} x {$api->SystemItemIDtoName(227)}(s) for logging into Chivalry is Dead today. Thanks a lot!");
+        $api->SystemLogsAdd($userid, "loginreward", "Received {$skill} x {$api->SystemItemIDtoName(227)}(s)");
     }
     if ($ir['vip_days'] > 0)
     {
