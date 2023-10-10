6ef48cebde00e11c9168f0645583128f5b600ffa
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-10-09 21:03:21 -0400

    Update enemy.php

--- upload/enemy.php
+++ upload/enemy.php
@@ -33,20 +33,13 @@ function enemy_list()
 	$ir['friend_count']=$db->fetch_single($db->query("/*qc=on*/SELECT COUNT(`enemy_id`) FROM `enemy` WHERE `enemy_user` = {$userid}"));
     echo "
 [<a href='?action=add'>Add an Enemy</a>] || [<a href='?action=spy'>Hire Spy</a>]<br />
-These are the people on your enemy list. " . number_format($ir['friend_count']) . " player(s) have added you to their enemy list.
+
 <br />
-<table class='table table-bordered table-striped'>
-		<thead>
-		<tr>
-			<th>Name</th>
-			<th>Mail</th>
-			<th>Attack</th>
-			<th>Remove</th>
-			<th>Comment</th>
-			<th>Status</th>
-		</tr>
-		</thead>
-		<tbody>
+        <div class='card'>
+        <div class='card-header'>
+            These are the people on your enemy list. " . shortNumberParse($ir['friend_count']) . " player(s) have added you to their enemy list.
+        </div>
+        <div class='card-body'>
    ";
     $q =
             $db->query(
@@ -71,28 +64,71 @@ These are the people on your enemy list. " . number_format($ir['friend_count']) 
         {
             $attacklink="<a href='attack.php?user={$r['userid']}'>Attack</a>";
         }
-        $on =
-                ($r['laston'] >= $laston)
-                        ? '<span class="text-success">Online</font>'
-                        : '<span class="text-danger">Offline</font>';
+        $on = parseActivity($r['userid']);
         $r['username'] = parseUsername($r['userid']);
         if (!$r['comment'])
         {
             $r['comment'] = 'N/A';
         }
         echo "
-		<tr>
-			<td><a href='profile.php?user={$r['userid']}'>{$r['username']}</a> [{$r['userid']}]</td>
-			<td><a href='inbox.php?action=compose&user={$r['userid']}'>Mail</a></td>
-			<td>{$attacklink}</td>
-			<td><a href='?action=remove&f={$r['enemy_id']}'>Remove</a></td>
-			<td>" . strip_tags($r['comment']) . "<br />[<a href='?action=ccomment&f={$r['enemy_id']}'>Change</a>]</td>
-			<td>$on</td>
-		</tr>
-   ";
+        <div class='row'>
+            <div class='col-xl col-auto'>
+                <div class='row'>
+                    <div class='col-12'>
+                        <small><b>Name</b></small>
+                    </div>
+                    <div class='col-12'>
+                        <a href='profile.php?user={$r['userid']}'>{$r['username']}</a> " . parseUserID($r['userid']) . "
+                    </div>
+                </div>
+            </div>
+            <div class='col-xl col-auto'>
+                <div class='row'>
+                    <div class='col-12'>
+                        <small><b>Actions</b></small>
+                    </div>
+                    <div class='col-12'>
+                        <div class='row'>
+                            <div class='col-auto'>
+                                " . createPrimaryBadge("<a href='inbox.php?action=compose&user={$r['userid']}'>Mail</a>") . "
+                            </div>
+                            <div class='col-auto'>
+                                " . createDangerBadge($attacklink) . "
+                            </div>
+                            <div class='col-auto'>
+                                " . createSuccessBadge("<a href='?action=remove&f={$r['enemy_id']}'>Remove</a>") . "
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='col-xl col-auto'>
+                <div class='row'>
+                    <div class='col-12'>
+                        <small><b>Comment</b></small>
+                    </div>
+                    <div class='col-12'>
+                        " . strip_tags($r['comment']) . "
+                    </div>
+                    <div class='col-12'>
+                        <small><a href='?action=ccomment&f={$r['enemy_id']}'>Change</a></small>
+                    </div>
+                </div>
+            </div>
+            <div class='col-xl col-auto'>
+                <div class='row'>
+                    <div class='col-12'>
+                        <small><b>Activity</b></small>
+                    </div>
+                    <div class='col-12'>
+                        $on
+                    </div>
+                </div>
+            </div>
+        </div>";
     }
     $db->free_result($q);
-    echo "</tbody></table>";
+    echo "</div></div>";
 	$h->endpage();
 }
 function add_friend()
@@ -137,7 +173,8 @@ function add_friend()
             $db->query(
                     "INSERT INTO `enemy`
                      VALUES(NULL,  {$_POST['ID']}, {$userid}, '{$_POST['comment']}')");
-			alert('success',"Success!","You have successfully added {$api->SystemUserIDtoName($_POST['ID'])} to your enemy list.",true,'enemy.php');
+			alert('success',"Success!","You have successfully added {$api->SystemUserIDtoName($_POST['ID'])} to your enemy list.", false);
+			enemy_list();
         }
         $db->free_result($q);
     }
@@ -146,36 +183,47 @@ function add_friend()
         $_GET['ID'] =
                 (isset($_GET['ID']) && is_numeric($_GET['ID']))
                         ? abs(intval($_GET['ID'])) : '';
-		echo "<table class='table table-bordered'>
-		<form method='post'>
-			<tr>
-				<th colspan='2'>
-					Select the user you wish to add to your enemy list. Feel free to give them a friendly comment.
-				</th>
-			</tr>
-			<tr>
-				<th>
-					Enemy
-				</th>
-				<td>
-					" . user_dropdown('ID',$_GET['ID']) ."
-				</td>
-			</tr>
-			<tr>
-				<th>
-					Comment
-				</th>
-				<td>
-					<input type='text' class='form-control' name='comment'>
-				</td>
-			</tr>
-			<tr>
-				<td colspan='2'>
-					<input type='submit' class='btn btn-primary' value='Add Enemy'>
-				</td>
-			</tr>
-		</form>
-		</table>";
+        echo "  <form method='post'>
+                <div class='card'>
+                    <div class='card-header'>
+                        Submit this form to add a user to your enemy list.
+                    </div>
+                    <div class='card-body'>
+                        <div class='row'>
+                            <div class='col'>
+                                <div class='row'>
+                                    <div class='col-12'>
+                                        <small><b>Enemy</b></small>
+                                    </div>
+                                    <div class='col-12'>
+                                        " . user_dropdown('ID',$_GET['ID']) ."
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col'>
+                                <div class='row'>
+                                    <div class='col-12'>
+                                        <small><b>Comment</b></small>
+                                    </div>
+                                    <div class='col-12'>
+                                        <input type='text' class='form-control' name='comment' placeholder='Optional'>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col'>
+                                <div class='row'>
+                                    <div class='col-12'>
+                                        <small><b>Action</b></small>
+                                    </div>
+                                    <div class='col-12'>
+                                        <input type='submit' class='btn btn-primary btn-block' value='Add Enemy'>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>";
     }
 	$h->endpage();
 }
@@ -206,11 +254,12 @@ function remove_friend()
             "DELETE FROM `enemy`
             WHERE `enemy_id` = {$_GET['f']} AND `enemy_adder` = $userid");
    alert('success',"Success!","You have successfully removed this enemy.",true,'enemy.php');
+   enemy_list();
    $h->endpage();
 }
 function change_comment()
 {
-    global $db, $ir, $c, $userid, $h;
+    global $db, $ir, $c, $userid, $h, $api;
     if (isset($_POST['f']))
     {
 		$_POST['f'] =
@@ -246,7 +295,7 @@ function change_comment()
         }
         $q =
                 $db->query(
-                        "/*qc=on*/SELECT `comment`
+                        "/*qc=on*/SELECT *
                          FROM `enemy`
                          WHERE `enemy_id` = {$_GET['f']}
                          AND `enemy_adder` = $userid");
@@ -257,34 +306,38 @@ function change_comment()
                     stripslashes(
                             htmlentities($r['comment'], ENT_QUOTES,
                                     'ISO-8859-1'));
-            echo "
-			<form method='post'>
-			<table class='table table-bordered'>
-			<input type='hidden' name='f' value='{$_GET['f']}'>
-				<thead>
-					<tr>
-						<th colspan='2'>
-						Changing friend's comment...
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th>
-							Comment
-						</th>
-						<td>
-							<input type='text' name='comment' class='form-control' value='{$comment}'>
-						</td>
-					</tr>
-					<tr>
-						<td colspan='2'>
-							<input type='submit' class='btn btn-primary' value='Change Comment' />
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			</form>";
+                            echo "  <form method='post'>
+                <div class='card'>
+                    <div class='card-header'>
+                        Submit this form to change the comment you have for {$api->SystemUserIDtoName($r['enemy_user'])} " . parseUserID($r['enemy_user']) . "
+                    </div>
+                    <div class='card-body'>
+                        <div class='row'>
+                            <div class='col'>
+                                <div class='row'>
+                                    <div class='col-12'>
+                                        <small><b>Comment</b></small>
+                                    </div>
+                                    <div class='col-12'>
+                                        <input type='text' class='form-control' name='comment' placeholder='Optional'>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col'>
+                                <div class='row'>
+                                    <div class='col-12'>
+                                        <small><b>Action</b></small>
+                                    </div>
+                                    <div class='col-12'>
+                                        <input type='submit' class='btn btn-primary btn-block' value='Add Enemy'>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <input type='hidden' name='f' value='{$_GET['f']}'>
+            </form>";
         }
         else
         {
@@ -296,7 +349,7 @@ function change_comment()
 function spy()
 {
 	global $db, $userid, $api, $h, $ir;
-	echo "<h3>Hire Spy</h3>";
+	$spycost = 5000000 * levelMultiplier($ir['level'], $ir['reset']);
 	//Block access if user is in the infirmary.
 	if ($api->UserStatus($ir['userid'], 'infirmary')) {
 		alert('danger', "Unconscious!", "You cannot hire a spy while in the infirmary.", false);
@@ -323,20 +376,20 @@ function spy()
         {
             alert('danger',"Uh Oh!","You are trying to spy on a non-existent user.");
         }
-		elseif ($ir['primary_currency'] < 5000000)
+		elseif ($ir['primary_currency'] < $spycost)
 		{
-			alert('danger',"Uh Oh!","You do not have enough Copper Coins to hire this spy. You need 5,000,000 
-			Copper Coins and you've only got " . number_format($ir['primary_currency']) . " Copper Coins.");
+		    alert('danger',"Uh Oh!","You do not have enough Copper Coins to hire this spy. You need " . shortNumberParse($spycost) . " 
+			Copper Coins and you've only got " . shortNumberParse($ir['primary_currency']) . " Copper Coins.");
 		}
 		else
         {
-			$api->UserTakeCurrency($userid,'primary',5000000);
+            $api->UserTakeCurrency($userid,'primary', $spycost);
             $rng = Random(1,10);
-			if ($rng <= 4)
+			if ($rng <= 2)
 			{
 				alert('danger',"Uh Oh!","Your spy took your Copper Coins and ran. Wow... what the fuck.");
 			}
-			elseif (($rng <= 8) && ($rng > 4))
+			elseif (($rng <= 4) && ($rng > 2))
 			{
 				$time = $ir['level'] * (Random(1,5));
 				alert('danger',"Uh Oh!","Your spy took the money... but then smacked you out cold. You wake up to find out he was a dungeon guard in disguise... and in a dungeon cell...");
@@ -362,13 +415,42 @@ function spy()
 	}
 	else
 	{
-		echo "This spy is a little different. You may use this spy to find out if someone has added you to their enemy list. This spy costs a flat fee 
-		of 5,000,000 Copper Coins.<br />
-		<u>Just select the user you're curious if they've added you to their enemy list, and a spy will attempt to let you know.</u>
-		<form method='post'>
-			" . user_dropdown('userid') . "
-			<input type='submit' class='btn btn-primary'>
-		</form>";
+        echo "      <form method='post'>
+                    <div class='card'>
+                        <div class='card-header'>
+                            Enemy Spy
+                        </div>
+                        <div class='card-body'>
+                            <div class='row'>
+                                <div class='col-12'>
+                                    This spy will confirm whether or not the selected user has added you to their enemy list. This spy costs " . shortNumberParse($spycost) . " Copper Coins.
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='col'>
+                                    <div class='row'>
+                                        <div class='col-12'>
+                                            <small><b>Potential Enemy</b></small>
+                                        </div>
+                                        <div class='col-12'>
+                                            " . user_dropdown('userid') . "
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class='col'>
+                                    <div class='row'>
+                                        <div class='col-12'>
+                                            <small><b>Action</b></small>
+                                        </div>
+                                        <div class='col-12'>
+                                            <input type='submit' class='btn btn-primary btn-block' value='Hire Spy'>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </form>";
 	}
 	$h->endpage();
 }
