311bf857d661f28453bbe98a65e35264cd8d5fa5
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2025-05-31 07:31:50 -0400

    Use emoji on profile buttons

--- upload/profile.php
+++ upload/profile.php
@@ -323,47 +323,47 @@ echo "<h3>{$user_name}'s Profile</h3>
 						<div id='profileTxt' class='text-left'>What would you like to do?</div><br />
 						<div class='row'>
 							<div class='col-auto'>
-								<a href='attack.php?user={$r['userid']}' onmouseover='profileButtonAttack(\"{$r['username']}\", {$attbutton})' class='btn btn-danger btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/attack-btn.svg", 1.75) . "</a><br />
+								<a href='attack.php?user={$r['userid']}' onmouseover='profileButtonAttack(\"{$r['username']}\", {$attbutton})' class='btn btn-danger btn-block' style='font-size: 1.75rem;'>⚔️</a><br />
 							</div>";
 						      if (currentMonth() == 10)
 						      {
     						       echo"<div class='col-auto'>
-    								<a href='halloween.php?action=tnt&user={$r['userid']}' onmouseover='profileButtonHalloweenVisit(\"{$r['username']}\")' class='btn btn-warning btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/halloween-btn.svg", 1.75) . "</a><br />
+    								<a href='halloween.php?action=tnt&user={$r['userid']}' onmouseover='profileButtonHalloweenVisit(\"{$r['username']}\")' class='btn btn-warning btn-block' style='font-size: 1.75rem;'>🎃</a><br />
     							</div>";   
 						      }
 						    echo"
 							<div class='col-auto'>
-								<a href='sendcash.php?user={$r['userid']}' onmouseover='profileButtonCash(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/send-cash-btn.svg", 1.75) . "</a><br />
+								<a href='sendcash.php?user={$r['userid']}' onmouseover='profileButtonCash(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>💰</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='inbox.php?action=compose&user={$r['userid']}' onmouseover='profileButtonMail(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/inbox-btn.svg", 1.75) . "</a><br />
+								<a href='inbox.php?action=compose&user={$r['userid']}' onmouseover='profileButtonMail(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>✉️</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='hirespy.php?user={$r['userid']}' onmouseover='profileButtonSpy(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/spy-btn.svg", 1.75) . "</a><br />
+								<a href='hirespy.php?user={$r['userid']}' onmouseover='profileButtonSpy(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>🕵️‍♂️</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='bounty.php?action=addbounty&user={$r['userid']}' onmouseover='profileButtonBounty(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("explore/bounty_hunter.svg", 1.75) . "</a><br />
+								<a href='bounty.php?action=addbounty&user={$r['userid']}' onmouseover='profileButtonBounty(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>⚰️</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='theft.php?user={$r['userid']}' onmouseover='profileButtonTheft(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/rob-btn.svg", 1.75) . "</a><br />
+								<a href='theft.php?user={$r['userid']}' onmouseover='profileButtonTheft(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>🤦</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='poke.php?user={$r['userid']}' onmouseover='profileButtonPoke(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/poke-btn.svg", 1.75) . "</a><br />
+								<a href='poke.php?user={$r['userid']}' onmouseover='profileButtonPoke(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>👆</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='friends.php?action=add&ID={$r['userid']}' onmouseover='profileButtonFriend(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/friend-btn.svg", 1.75) . "</a><br />
+								<a href='friends.php?action=add&ID={$r['userid']}' onmouseover='profileButtonFriend(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>👍</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='enemy.php?action=add&user={$r['userid']}' onmouseover='profileButtonEnemy(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/enemy-btn.svg", 1.75) . "</a><br />
+								<a href='enemy.php?action=add&user={$r['userid']}' onmouseover='profileButtonEnemy(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>👎</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='contacts.php?action=add&user={$r['userid']}' onmouseover='profileButtonContact(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/contact-btn.svg", 1.75) . "</a><br />
+								<a href='contacts.php?action=add&user={$r['userid']}' onmouseover='profileButtonContact(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>➕</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='blocklist.php?action=add&user={$r['userid']}' onmouseover='profileButtonBlock(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/block-btn.svg", 1.75) . "</a><br />
+								<a href='blocklist.php?action=add&user={$r['userid']}' onmouseover='profileButtonBlock(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>🛑</a><br />
 							</div>
 							<div class='col-auto'>
-								<a href='playerreport.php?userid={$r['userid']}' onmouseover='profileButtonReport(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>" . loadImageAsset("menu/profile/report-btn.svg", 1.75) . "</a><br />
+								<a href='playerreport.php?userid={$r['userid']}' onmouseover='profileButtonReport(\"{$r['username']}\")' class='btn btn-primary btn-block' style='font-size: 1.75rem;'>⚠️</a><br />
 							</div>
 						</div>
 					</div>
