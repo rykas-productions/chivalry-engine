1aa4a6b7dba07feb2750ce4cfd044f74d7617027
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2025-08-09 04:56:07 -0400

    Update installer.php

--- upload/installer.php
+++ upload/installer.php
@@ -11,8 +11,8 @@ if (file_exists('./installer.lock'))
 {
     exit;
 }
-$Version=('1.1.0'); //For game/engine
-$Build=('108');	    //todo: at 1.1, count the actual builds and start there. at 1.0.3c, we'd
+$Version=('1.1.1'); //For game/engine
+$Build=('109');	    //todo: at 1.1, count the actual builds and start there. at 1.0.3c, we'd
 					//actually be at 107
 define('MONO_ON', 1);
 session_name('CENGINE');
@@ -141,7 +141,7 @@ function diagnostics()
     <h3>Basic Diagnostic Results:</h3>
     <table class='table table-bordered table-hover'>
     		<tr>
-    			<td>Is the server's PHP Version >= 7.4?</td>
+    			<td>Is the server's PHP Version >= 7.3?</td>
     			<td>{$pv}</td>
     		</tr>
     		<tr>
