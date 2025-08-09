6f4cd19da4f137914a71e9e6954f42da0159db3c
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2025-08-09 03:48:11 -0400

    Update globals.php

--- upload/globals.php
+++ upload/globals.php
@@ -65,6 +65,16 @@ ob_start();
 require "lib/basic_error_handler.php";
 require "lib/dev_help.php";
 set_error_handler('error_php');
+
+set_exception_handler(function($exception) {
+    error_critical(
+        'An unexpected error occurred.',
+        $exception->getMessage(),
+        'Exception in ' . $exception->getFile() . ' on line ' . $exception->getLine(),
+        ['stack_trace' => $exception->getTraceAsString()]
+        );
+});
+
 //Require main functions file.
 require "global_func.php";
 $domain = determine_game_urlbase();
