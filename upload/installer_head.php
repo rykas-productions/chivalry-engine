d6e1cfc870e9834eaeb85cf68293cf30cd5ea481
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2026-08-16 19:55:28 -0400

    Include CE version in installer title

--- upload/installer_head.php
+++ upload/installer_head.php
@@ -20,7 +20,7 @@ ob_start();
 			<meta charset="utf-8">
 			<meta http-equiv="X-UA-Compatible" content="IE=edge">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<title>Chivalry Engine Installer</title>
+			<title>Chivalry Engine v<?php echo $Version; ?> Installer</title>
 			<!-- CSS -->
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 			<meta name="theme-color" content="#e7e7e7">
